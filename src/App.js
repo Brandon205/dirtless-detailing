c9e88ddb075fb105718c61c301785658dc30f800
Brandon Black <53108034+Brandon205@users.noreply.github.com>
2022-07-11 17:35:02 -0700

    Filling in real information

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import Home from './components/Home.js';
 import ddLogo from './imgs/dd-logo.png';
 
@@ -6,50 +7,54 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <nav>
-          <img src={ddLogo} className="logo" />
-          <FaBars className="menu-bars" />
-        </nav>
-      </header>
-      <main>
-        <Home />
-      </main>
-      <footer>
-        <div className="footer-content-card">
-          <h3>COMPANY</h3>
-          <p className="footer-text">Home</p>
-          <p className="footer-text">About Us</p>
-          <p className="footer-text">Contact</p>
-        </div>
-        <div className="footer-content-card">
-          <h3>EXTERIOR</h3>
-          <p className="footer-text">Premium Wash</p>
-          <p className="footer-text">Wash and Wax</p>
-          <p className="footer-text">Engine Cleaning</p>
-          <p className="footer-text">Paint Overspray Removal</p>
-          <p className="footer-text">Headlight Restoration</p>
-        </div>
-        <div className="footer-content-card">
-          <h3>INTERIOR</h3>
-          <p className="footer-text">Full Interior Cleaning</p>
-          <p className="footer-text">Restorative Interior Cleaning</p>
-          <p className="footer-text">Mini-Interior-Detail</p>
-          <p className="footer-text">Smoke Odor Removal</p>
-        </div>
-        <div className="footer-content-card">
-          <h3>CONTACT US</h3>
-          <p className="footer-text">Approximate Location here</p>
-          <p className="footer-text">(253) 252-9758</p>
-        </div>
-        <hr />
-        <div className='socials-container'>
-          <FaFacebook className="socials" />
-        </div>
+    <BrowserRouter>
+      <div className="App">
+        <header className="App-header">
+          <nav>
+            <img src={ddLogo} className="logo" />
+            <FaBars className="menu-bars" />
+          </nav>
+        </header>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+        </Routes>
+        <footer>
+          <div className="footer-content-card">
+            <h3>COMPANY</h3>
+            <p className="footer-text">Home</p>
+            <p className="footer-text">About Us</p>
+            <p className="footer-text">Contact</p>
+          </div>
+          <div className="footer-content-card">
+            <h3>INTERIOR</h3>
+            <p className="footer-text">Full Interior Cleaning with Extraction</p>
+            <p className="footer-text">Full Interior Cleaning without Extraction</p>
+            <p className="footer-text">Smoke / Biohazard / Mold Removal</p>
+            <p className="footer-text">Headliners</p>
+          </div>
+          <div className="footer-content-card">
+            <h3>EXTERIOR</h3>
+            <p className="footer-text">Dirt-less Wash</p>
+            <p className="footer-text">Premium Dirt-less Wash</p>
+            <p className="footer-text">Engine Cleaning</p>
+            <p className="footer-text">Single Stage Paint Correction</p>
+            <p className="footer-text">Two Stage Paint Correction</p>
+            <p className="footer-text">Glass Polishing</p>
+            <p className="footer-text">Waterspot / Overspray Removal</p>
+          </div>
+          <div className="footer-content-card">
+            <h3>CONTACT US</h3>
+            <a href="https://www.google.com/maps/place/9305+205th+Ave+E,+Bonney+Lake,+WA+98391/@47.172835,-122.1589741,17z/data=!3m1!4b1!4m5!3m4!1s0x5490faeb8aa2e3d7:0xe53c2e7cb4aa7549!8m2!3d47.1728314!4d-122.1567854" className="contact-link">9305 205th Ave E Bonney Lake Washington</a><br />
+            <a href="tel:2532529758" className="contact-link">(253) 252-9758</a>
+          </div>
+          <hr />
+          <div className='socials-container'>
+            <a href="https://www.facebook.com/DirtLessDetailing/" className="socials"><FaFacebook /></a>
+          </div>
 
-      </footer>
-    </div>
+        </footer>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../App.css';
 
 export default function Home() {
@@ -6,60 +7,105 @@ export default function Home() {
         <div className="App">
             <section>
                 <h2>Your Pierce County Car Detailing Specialists</h2>
-                <h4 className='dd-desc'>Over 100+ 5-star reviews from our happy customers. From interior cleaning to paint correction and ceramic coatings. We can handle all your detailing needs.</h4>
+                <h4 className='dd-desc'>Plenty of 5-star reviews from the customers we've served. We will do Interiors, Exteriors, Paint Correction, and much more. We will have your car looking brand new.</h4>
                 <button className="learn-btn">Learn More</button>
-                <button className="quote-btn">Get Quote</button>
+                <button className="quote-btn">Get a Quote</button>
             </section>
             <section className="contact-container">
                 <h3>Contact Us</h3>
-                <p className="phone">(253) 252-9758</p>
+                <a href="tel:2532529758" className="contact-link">(253) 252-9758</a>
 
                 <h3>WORKING HOURS</h3>
-                <p className="hours-p">Tue-Fri: 9 AM - 5 PM</p>
-                <p className="hours-p">Saturday: 9 AM - 2 PM</p>
-                <p className="hours-p">Sunday/Monday: Closed</p>
+                <p className="hours-p">Monday - Sunday: 8 AM - 8 PM</p>
 
                 <h3>LOCATION</h3>
-                Pierce County Washington
+                {/* <p>9305 205th Ave E Bonney Lake Washington</p> */}
+                <a href="https://www.google.com/maps/place/9305+205th+Ave+E,+Bonney+Lake,+WA+98391/@47.172835,-122.1589741,17z/data=!3m1!4b1!4m5!3m4!1s0x5490faeb8aa2e3d7:0xe53c2e7cb4aa7549!8m2!3d47.1728314!4d-122.1567854" className="contact-link">9305 205th Ave E Bonney Lake Washington</a>
             </section>
             <section className="popular-container">
                 <h2>Popular Detailing Services</h2>
 
                 <div className="popular-services-card">
-                    <h3>Full Interior Cleaning</h3>
+                    <h3 className="popular-service-title">Full Interior Cleaning with Extraction</h3>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud</p>
                     <p>exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur</p>
                     <p>sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                     <button className="readmore">Read More</button>
                 </div>
                 <div className="popular-services-card">
-                    <h3>Paint Correction</h3>
+                    <h3 className="popular-service-title">Two Stage Paint Correction</h3>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud</p>
                     <p>exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur</p>
                     <p>sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                     <button className="readmore">Read More</button>
                 </div>
                 <div className="popular-services-card">
-                    <h3>Wash and Wax</h3>
+                    <h3 className="popular-service-title">Premium Dirt-less Wash</h3>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud</p>
                     <p>exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur</p>
                     <p>sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                     <button className="readmore">Read More</button>
                 </div>
 
-                <button className="quote-btn">Get Quote</button>
             </section>
+
+            <button className="quote-btn">Get a Quote</button>
 
             <section className="otherservices-container">
                 <h2>Our Other Services</h2>
 
+                <h3>INTERIOR CLEANING</h3>
                 <div className="service-card">
-                    <h3>Ceramic Coating</h3>
+                    <h3>Full Interior without Extraction</h3>
                     <p className="service-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
                     <button className="readmore">Read More</button>
                 </div>
                 <div className="service-card">
-                    <h3>Premium Wash</h3>
+                    <h3 className="popular-service-title">Full Interior with Extraction</h3>
+                    <p className="service-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    <button className="readmore">Read More</button>
+                </div>
+                <div className="service-card">
+                    <h3>Smoker Car/Bio/Mold or other Extreme Condition Cleaning</h3>
+                    <p className="service-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    <button className="readmore">Read More</button>
+                </div>
+                <div className="service-card">
+                    <h3>Headliners</h3>
+                    <p className="service-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    <button className="readmore">Read More</button>
+                </div>
+
+                <h3>EXTERIOR CORRECTION</h3>
+                <div className="service-card">
+                    <h3>Single Stage Paint Correction</h3>
+                    <p className="service-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    <button className="readmore">Read More</button>
+                </div>
+                <div className="service-card">
+                    <h3 className="popular-service-title">Two Stage Paint Correction</h3>
+                    <p className="service-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    <button className="readmore">Read More</button>
+                </div>
+                <div className="service-card">
+                    <h3>Glass Polishing</h3>
+                    <p className="service-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    <button className="readmore">Read More</button>
+                </div>
+                <div className="service-card">
+                    <h3>Waterspot and Overspray Removal</h3>
+                    <p className="service-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    <button className="readmore">Read More</button>
+                </div>
+
+                <h3>EXTERIOR CLEANING</h3>
+                <div className="service-card">
+                    <h3>Dirt-less Wash</h3>
+                    <p className="service-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    <button className="readmore">Read More</button>
+                </div>
+                <div className="service-card">
+                    <h3 className="popular-service-title">Premium Dirt-less Wash</h3>
                     <p className="service-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
                     <button className="readmore">Read More</button>
                 </div>
@@ -68,20 +114,10 @@ export default function Home() {
                     <p className="service-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
                     <button className="readmore">Read More</button>
                 </div>
-                <div className="service-card">
-                    <h3>Mini-Interior Detail</h3>
-                    <p className="service-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                    <button className="readmore">Read More</button>
-                </div>
-                <div className="service-card">
-                    <h3>Smoke Odor Removal</h3>
-                    <p className="service-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                    <button className="readmore">Read More</button>
-                </div>
             </section>
             <section className="serviceareas-container">
-                <h2>Servicing the Pierce County and King County Area</h2>
-                <p>We can come to you, all we need is access to your vehicle and we're ready to work. Simple and Hassle Free</p>
+                <h2>Servicing most of the Pierce County and King County Area</h2>
+                <p>As part of our simple and hassle free service, we can come to you, all we need is access to your vehicle and we'll get to work.</p>
             </section>
         </div>
     )
