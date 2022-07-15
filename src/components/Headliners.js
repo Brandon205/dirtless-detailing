2a08e978e337c011105328396ad34e88f7b3f4e3
Brandon Black <53108034+Brandon205@users.noreply.github.com>
2022-07-14 20:35:01 -0700

    Interior base pages made

--- src/components/Headliners.js
+++ src/components/Headliners.js
@@ -1,7 +1,39 @@
-import React from 'react'
+import React from 'react';
+
+import cover from '../assets/imgs/fullIntExCover.jpg';
+
+const steps = [
+  "We will first determine what fabric and materials are present to find the lightest form of cleaning to start with.",
+  "We will perform the cleaning methods found from the first step, we do this to prevent sagging and damage to the headliners. (results may vary)"
+]
 
 export default function Headliners() {
+  const content = steps.map((item, id) => {
+    return (
+      <li key={id} className="item">
+        <h3>Step {id + 1}:</h3>
+        <p>{item}</p>
+      </li>
+    )
+  })
   return (
-    <div>Headliners</div>
+    <section className='content-container'>
+      <div className="cover" style={{backgroundImage: 'url(' + cover + ')' }}></div>
+      <h1 className='servicepage-heading'>Headliners</h1>
+      <div className='main-content-container'>
+        <h2>Our Headliner Cleaning Process</h2>
+        <ol className='list'>
+          {content}
+        </ol>
+        <h2>Pricing</h2>
+        <h4>Our basic prices are shown below, actual prices will be based on your vehicle size.</h4>
+        <div className="pricing-container">
+          <div className="pricing-card">
+            <p className="example-vehicle">Any size vehicle, price will vary on products needed and cleanlyness of the headliners</p>
+            <p className="pricing-price">$40-$80</p>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
