a213dfd0c77aef80ec91e584398a3b65cd6f2ca3
Brandon Black <brandonblack02@gmail.com>
2022-08-29 19:20:56 -0700

    Updated Bio page

--- src/components/pages/Bio.js
+++ src/components/pages/Bio.js
@@ -6,16 +6,21 @@ import { BsCheck2Circle } from 'react-icons/bs';
 import cover from '../../assets/imgs/fullIntExCover.jpg';
 
 const steps = [
-  "We will first determine what products will need to be used to best suit the situation.",
-  "Then we will perform Ozone Air treatment to fill all bacteria, mold spores, and get rid of smells where it's impossible to get to conventionally."
+  ["Determine Products", "We will first determine what products will need to be used to best suit the situation."],
+  ["Ozone Treatment", "Then we will perform Ozone Air treatment to fill all bacteria, mold spores, and get rid of smells where it's impossible to get to conventionally."]
 ]
 
 export default function Bio() {
   const content = steps.map((item, id) => {
     return (
       <li key={id} className="item">
-        <h3>Step {id + 1}:</h3>
-        <p>{item}</p> 
+        <div className="step-number-container">
+          <h3 className='step-number'>{id + 1}</h3><div className='vertical-rule'></div>
+        </div>
+        <div className="step-container">
+          <h4 className="step-title">{item[0]}</h4>
+          <p className='step-desc'>{item[1]}</p> 
+        </div>
       </li>
     )
   });
@@ -26,13 +31,15 @@ export default function Bio() {
       <h1 className='servicepage-header'>Smoke/Biohazard/Odor Removal</h1>
 
       <div className='main-content-container'>
-        <div>
-          <h2>Our Biohazard Cleaning Process</h2>
-          <ol className='list'>
-            {content}
-          </ol>
+        <div className='process-container'>
+          <div>
+            <h2>Our Biohazard Cleaning Process</h2>
+            <ol className='list'>
+              {content}
+            </ol>
+          </div>
 
-          <div className="service-checklist">
+          {/* <div className="service-checklist">
             <h3 className="checklist-title">Biohazard Cleaning</h3>
             <h4>This Package Includes:</h4>
             <ul className="checklist">
@@ -45,10 +52,24 @@ export default function Bio() {
                 <p>Treating the areas to eliminate all issues</p>
               </li>
             </ul>
-          </div>
+          </div> */}
 
-          <h2>Bio Hazard Cleaning Pricing</h2>
-          <h4>Our basic prices are shown below, actual prices will be based on your vehicle size.</h4>
+          <div className='aside-container'>
+            <div className='service-aside'>
+              <h3>Other Interior Services</h3>
+              <div className="aside-links">
+                <Link to="/full-int-ex" style={{color: window.location.href.endsWith('full-int-ex') ? 'blue' : 'black'}} className='service-link'>{window.location.href.endsWith('full-int-ex') ? '> ' : ''} Full Interior Cleaning</Link><br />
+                <Link to="/bio" style={{color: window.location.href.endsWith('bio') ? 'blue' : 'black'}} className='service-link'>{window.location.href.endsWith('bio') ? '> ' : ''} Bio Hazard/Odor Cleaning</Link><br />
+                <Link to="/headliners" style={{color: window.location.href.endsWith('headliners') ? 'blue' : 'black'}} className={'service-link'}>{window.location.href.endsWith('headliners') ? '> ' : ''} Headliners</Link>
+              </div>
+            </div>
+            <HashLink smooth to="/#contact" className="quote-btn top-quote-btn service-page-quote-btn">Contact Us</HashLink>
+          </div>
+        </div>
+
+        <div>
+          <h2 className='pricing-title'>Bio Hazard Cleaning Pricing</h2>
+          <h4 className='subtext'>Our basic prices are shown below, actual prices will be based on your vehicle size.</h4>
           <div className="pricing-container">
             <div className="pricing-card">
               <p className="example-vehicle">Any vehicle size, price will vary based on the situation</p>
@@ -56,18 +77,7 @@ export default function Bio() {
             </div>
           </div>
         </div>
-
-        <div className='aside-container'>
-          <div className='service-aside'>
-            <h3>Other Interior Services</h3>
-            <div className="aside-links">
-              <Link to="/full-int-ex" style={{color: window.location.href.endsWith('full-int-ex') ? 'blue' : 'black'}} className='service-link'>{window.location.href.endsWith('full-int-ex') ? '> ' : ''} Full Interior Cleaning</Link><br />
-              <Link to="/bio" style={{color: window.location.href.endsWith('bio') ? 'blue' : 'black'}} className='service-link'>{window.location.href.endsWith('bio') ? '> ' : ''} Bio Hazard/Odor Cleaning</Link><br />
-              <Link to="/headliners" style={{color: window.location.href.endsWith('headliners') ? 'blue' : 'black'}} className={'service-link'}>{window.location.href.endsWith('headliners') ? '> ' : ''} Headliners</Link>
-            </div>
-          </div>
-          <HashLink smooth to="/#contact" className="quote-btn top-quote-btn service-page-quote-btn">Contact Us</HashLink>
-        </div>
+        
       </div>
     </section>
   )
