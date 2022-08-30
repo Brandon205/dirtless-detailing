3f3702a831a2d5f2b4f0f60f45d722ce0a451653
Brandon Black <brandonblack02@gmail.com>
2022-08-29 20:29:43 -0700

    Updated Headliners page

--- src/components/pages/Headliners.js
+++ src/components/pages/Headliners.js
@@ -1,41 +1,65 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { HashLink } from 'react-router-hash-link';
 
 import cover from '../../assets/imgs/fullIntExCover.jpg';
 
 const steps = [
-  "We will first determine what fabric and materials are present to find the lightest form of cleaning to start with.",
-  "We will perform the cleaning methods found from the first step, we do this to prevent sagging and damage to the headliners. (results may vary)"
+  ["Determine Method", "We will first determine what fabric and materials are present to find the lightest form of cleaning to start with."],
+  ["Clean Headliners", "We will perform the cleaning methods found from the first step, we do this to prevent sagging and damage to the headliners. (results may vary)"]
 ]
 
 export default function Headliners() {
   const content = steps.map((item, id) => {
     return (
       <li key={id} className="item">
-        <h3>Step {id + 1}:</h3>
-        <p>{item}</p>
+        <div className="step-number-container">
+          <h3 className='step-number'>{id + 1}</h3><div className="vertical-rule"></div>
+        </div>
+        <div className="step-container">
+          <h4 className="step-title">{item[0]}</h4>
+          <p className='step-desc'>{item[1]}</p>
+        </div>
       </li>
     )
   })
   return (
-    <section className='content-container'>
+    <section className='service-content-container'>
       <div className="cover" style={{backgroundImage: 'url(' + cover + ')' }}></div>
-      <h1 className='servicepage-heading'>Headliners</h1>
+      <h1 className='servicepage-header'>Headliners</h1>
+
       <div className='main-content-container'>
-        <h2>Our Headliner Cleaning Process</h2>
-        <ol className='list'>
-          {content}
-        </ol>
-        <h2>Pricing</h2>
-        <h4>Our basic prices are shown below, actual prices will be based on your vehicle size.</h4>
-        <div className="pricing-container">
-          <div className="pricing-card">
-            <p className="example-vehicle">Any size vehicle, price will vary on products needed and cleanlyness of the headliners</p>
-            <p className="pricing-price">$40-$80</p>
+        <div className="process-container">
+          <div>
+            <h2>Our Headliner Cleaning Process</h2>
+            <ol className='list'>
+              {content}
+            </ol>
+          </div>
+
+          <div className='aside-container'>
+            <div className='service-aside'>
+              <h3>Our Other Interior Services</h3>
+              <div className="aside-links">
+                <Link to="/full-int-ex" style={{color: window.location.href.endsWith('full-int-ex') ? 'blue' : 'black'}} className='service-link'>{window.location.href.endsWith('full-int-ex') ? '> ' : ''} Full Interior Cleaning</Link><br />
+                <Link to="/bio" style={{color: window.location.href.endsWith('bio') ? 'blue' : 'black'}} className='service-link'>{window.location.href.endsWith('bio') ? '> ' : ''} Bio Hazard/Odor Cleaning</Link><br />
+                <Link to="/headliners" style={{color: window.location.href.endsWith('headliners') ? 'blue' : 'black'}} className={'service-link'}>{window.location.href.endsWith('headliners') ? '> ' : ''} Headliners</Link>
+              </div>
+            </div>
+            <HashLink smooth to="/#contact" className="quote-btn top-quote-btn service-page-quote-btn">Contact Us</HashLink>
+          </div>
+        </div>
+        <div>
+          <h2>Headliner Cleaning Pricing</h2>
+          <h4>Our basic prices are shown below, actual prices will be based on your vehicle size.</h4>
+          <div className="pricing-container">
+            <div className="pricing-card">
+              <p className="example-vehicle">Any size vehicle, price will vary on products needed and cleanlyness of the headliners</p>
+              <p className="pricing-price">$40-$80</p>
+            </div>
           </div>
         </div>
       </div>
-      <HashLink smooth to="/#contact" className="quote-btn top-quote-btn service-page-quote-btn">Get Quote</HashLink>
     </section>
   )
 }
