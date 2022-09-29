10a28c93d1968455fdfe582125b7cf6911966eff
Brandon Black <53108034+Brandon205@users.noreply.github.com>
2022-09-29 15:49:34 -0700

    Working on popular service cards

--- src/components/Home.js
+++ src/components/Home.js
@@ -141,8 +141,10 @@ export default function Home() {
             <section className="popular-container">
 
                 <div className="popular-services-card">
-                    <Link to="full-int-ex" className="service-heading-link"><h2 className="popular-service-title">Full Interior Cleaning</h2></Link>
-                    <p className="price">Starting at: <span>$200</span></p>
+                    <div className="popular-services-header">
+                        <Link to="full-int-ex" className="service-heading-link"><h2 className="popular-service-title">Full Interior Cleaning</h2></Link>
+                        <p className="price">Starting at: <span>$200</span></p>
+                    </div>
                     <img src={interior} alt="car interior before and after" className="popular-img" />
 
                     <div className="popular-desc">
@@ -151,30 +153,30 @@ export default function Home() {
                                 <BsCheck2Circle className='checkcircle' />
                                 <p>Blow out with Vortex Blowgun</p>
                             </li>
-                                <li className="checklist-item">
-                                    <BsCheck2Circle className='checkcircle' />
-                                    <p>Hot Water Extraction - If added</p>
-                                </li>
-                                <li className="checklist-item">
-                                    <BsCheck2Circle className='checkcircle' />
-                                    <p>Vacuum the Whole Interior</p>
-                                </li>
-                                <li className="checklist-item">
-                                    <BsCheck2Circle className='checkcircle' />
-                                    <p>Clean ALL Plastics/Leather</p>
-                                </li>
-                                <li className="checklist-item">
-                                    <BsCheck2Circle className='checkcircle' />
-                                    <p>Dress Vinyl/Leather</p>
-                                </li>
-                                <li className="checklist-item">
-                                    <BsCheck2Circle className='checkcircle' />
-                                    <p>Clean Door Jambs</p>
-                                </li>
-                                <li className="checklist-item">
-                                    <BsCheck2Circle className='checkcircle' />
-                                    <p>Clean Windows</p>
-                                </li>
+                            <li className="checklist-item">
+                                <BsCheck2Circle className='checkcircle' />
+                                <p>Hot Water Extraction - If added</p>
+                            </li>
+                            <li className="checklist-item">
+                                <BsCheck2Circle className='checkcircle' />
+                                <p>Vacuum the Whole Interior</p>
+                            </li>
+                            <li className="checklist-item">
+                                <BsCheck2Circle className='checkcircle' />
+                                <p>Clean ALL Plastics/Leather</p>
+                            </li>
+                            <li className="checklist-item">
+                                <BsCheck2Circle className='checkcircle' />
+                                <p>Dress Vinyl/Leather</p>
+                            </li>
+                            <li className="checklist-item">
+                                <BsCheck2Circle className='checkcircle' />
+                                <p>Clean Door Jambs</p>
+                            </li>
+                            <li className="checklist-item">
+                                <BsCheck2Circle className='checkcircle' />
+                                <p>Clean Windows</p>
+                            </li>
                         </ul>
                     </div>
                     <Link to="full-int-ex" className="popular-readmore">
@@ -183,8 +185,10 @@ export default function Home() {
                 </div>
 
                 <div className="popular-services-card">
-                    <Link to="premium-dl-wash" className="service-heading-link"><h2 className="popular-service-title">Premium Dirt-Less Wash</h2></Link>
-                    <p className="price">Starting at: <span>$150</span></p>
+                    <div className="popular-services-header">
+                        <Link to="premium-dl-wash" className="service-heading-link"><h2 className="popular-service-title">Premium Dirt-Less Wash</h2></Link>
+                        <p className="price">Starting at: <span>$150</span></p>
+                    </div>
                     <img src={dlWash} alt="car exterior" className="popular-img" />
                     
                     <div className="popular-desc">
@@ -193,30 +197,30 @@ export default function Home() {
                                 <BsCheck2Circle className='checkcircle' />
                                 <p>Full Vehicle Strip Wash</p>
                             </li>
-                                <li className="checklist-item">
-                                    <BsCheck2Circle className='checkcircle' />
-                                    <p>Clean Wheel Wells, Wheel Barriers, and Wheels</p>
-                                </li>
-                                <li className="checklist-item">
-                                    <BsCheck2Circle className='checkcircle' />
-                                    <p>Clay Bar Treatment</p>
-                                </li>
-                                <li className="checklist-item">
-                                    <BsCheck2Circle className='checkcircle' />
-                                    <p>Iron Decontamination Treatment</p>
-                                </li>
-                                <li className="checklist-item">
-                                    <BsCheck2Circle className='checkcircle' />
-                                    <p>Apply 6 month sealant or better</p>
-                                </li>
-                                <li className="checklist-item">
-                                    <BsCheck2Circle className='checkcircle' />
-                                    <p>Clean Door Jambs</p>
-                                </li>
-                                <li className="checklist-item">
-                                    <BsCheck2Circle className='checkcircle' />
-                                    <p>Dress Plastics, Wheel Wells, and Tires</p>
-                                </li>
+                            <li className="checklist-item">
+                                <BsCheck2Circle className='checkcircle' />
+                                <p>Clean Wheel Wells, Wheel Barriers, and Wheels</p>
+                            </li>
+                            <li className="checklist-item">
+                                <BsCheck2Circle className='checkcircle' />
+                                <p>Clay Bar Treatment</p>
+                            </li>
+                            <li className="checklist-item">
+                                <BsCheck2Circle className='checkcircle' />
+                                <p>Iron Decontamination Treatment</p>
+                            </li>
+                            <li className="checklist-item">
+                                <BsCheck2Circle className='checkcircle' />
+                                <p>Apply 6 month sealant or better</p>
+                            </li>
+                            <li className="checklist-item">
+                                <BsCheck2Circle className='checkcircle' />
+                                <p>Clean Door Jambs</p>
+                            </li>
+                            <li className="checklist-item">
+                                <BsCheck2Circle className='checkcircle' />
+                                <p>Dress Plastics, Wheel Wells, and Tires</p>
+                            </li>
                         </ul>
                     </div>
                     <Link to="premium-dl-wash" className="popular-readmore">
@@ -235,30 +239,30 @@ export default function Home() {
                                 <BsCheck2Circle className='checkcircle' />
                                 <p>Full Vehicle Strip Wash</p>
                             </li>
-                                <li className="checklist-item">
-                                    <BsCheck2Circle className='checkcircle' />
-                                    <p>Clean Wheel Wells, Wheel Barrels, and Wheels</p>
-                                </li>
-                                <li className="checklist-item">
-                                    <BsCheck2Circle className='checkcircle' />
-                                    <p>Clay Bar and Iron Decontamination Treatment</p>
-                                </li>
-                                <li className="checklist-item">
-                                    <BsCheck2Circle className='checkcircle' />
-                                    <p>Remove Tar and Sap</p>
-                                </li>
-                                <li className="checklist-item">
-                                    <BsCheck2Circle className='checkcircle' />
-                                    <p>Tape Off All Trim</p>
-                                </li>
-                                <li className="checklist-item">
-                                    <BsCheck2Circle className='checkcircle' />
-                                    <p>Single Stage Paint Correction</p>
-                                </li>
-                                <li className="checklist-item">
-                                    <BsCheck2Circle className='checkcircle' />
-                                    <p>Ceramic Spray/Coat</p>
-                                </li>
+                            <li className="checklist-item">
+                                <BsCheck2Circle className='checkcircle' />
+                                <p>Clean Wheel Wells, Wheel Barrels, and Wheels</p>
+                            </li>
+                            <li className="checklist-item">
+                                <BsCheck2Circle className='checkcircle' />
+                                <p>Clay Bar and Iron Decontamination Treatment</p>
+                            </li>
+                            <li className="checklist-item">
+                                <BsCheck2Circle className='checkcircle' />
+                                <p>Remove Tar and Sap</p>
+                            </li>
+                            <li className="checklist-item">
+                                <BsCheck2Circle className='checkcircle' />
+                                <p>Tape Off All Trim</p>
+                            </li>
+                            <li className="checklist-item">
+                                <BsCheck2Circle className='checkcircle' />
+                                <p>Single Stage Paint Correction</p>
+                            </li>
+                            <li className="checklist-item">
+                                <BsCheck2Circle className='checkcircle' />
+                                <p>Ceramic Spray/Coat</p>
+                            </li>
                         </ul>
                     </div>
                     <Link to="single-paint-correction" className="popular-readmore">
@@ -312,7 +316,7 @@ export default function Home() {
                 <div className="service-card">
                     <img src={bio} alt="biohazard cleaning before" className='otherservices-image' />
                     <div className="otherservices-desc">
-                        <Link to="bio" className="service-heading-link"><h2>Smoker Car/Bio/Mold/etc Cleaning</h2></Link>
+                        <Link to="bio" className="service-heading-link"><h2>Smokers Car/Bio/Mold/etc Cleaning</h2></Link>
                         <p className="secondary-text">Starting at: <span>$300</span></p>
                         <p className="service-desc">If there are bad smells like mold or smoke in your car this will be the package for you. We'll use cleaning chemicals suited for the job to get rid of those odors for good.</p>
 
