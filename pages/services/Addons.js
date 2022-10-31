12d7c0615e3257e7f56a90ce4a24822f67ecdc24
Brandon Black <53108034+Brandon205@users.noreply.github.com>
2022-10-31 12:06:32 -0700

    Base Addons page updates

--- pages/services/Addons.js
+++ pages/services/Addons.js
@@ -3,44 +3,44 @@ import Image from 'next/image';
 import Head from 'next/head';
 
 import cover from '../../public/imgs/addonsCover.webp';
+import interior from '../../public/imgs/pageimgs/homepage/fullintex.webp';
 
-const waterSpotSteps = [
-    ["Waterspot/Overspray Removal", "Remove all overspray and waterspots we can."]
-  ]
+// const waterSpotSteps = [
+//     ["Waterspot/Overspray Removal", "Remove all overspray and waterspots we can."]
+//   ]
 
-const glassSteps = [
-    ["Clean Glass", "Remove all scratches, etching, and watermarks possible."]
-]
+// const glassSteps = [
+//     ["Clean Glass", "Remove all scratches, etching, and watermarks possible."]
+// ]
 
 export default function Addons() {
-    const waterspotContent = waterSpotSteps.map((item, id) => {
-        return (
-            <li key={id} className="item">
-            <div className="step-number-container">
-                <h3 className='step-number'>{id + 1}</h3><div className="vertical-rule"></div>
-            </div>
-            <div className="step-container">
-                <h4 className="step-title">{item[0]}</h4>
-                <p className='step-desc'>{item[1]}</p>
-            </div>
-            </li>
-        )
-    })
+//     const waterspotContent = waterSpotSteps.map((item, id) => {
+//         return (
+//             <li key={id} className="item">
+//             <div className="step-number-container">
+//                 <h3 className='step-number'>{id + 1}</h3><div className="vertical-rule"></div>
+//             </div>
+//             <div className="step-container">
+//                 <h4 className="step-title">{item[0]}</h4>
+//                 <p className='step-desc'>{item[1]}</p>
+//             </div>
+//             </li>
+//         )
+//     })
 
-    const glassContent = glassSteps.map((item, id) => {
-        return (
-          <li key={id} className="item">
-            <div className="step-number-container">
-              <h3 className='step-number'>{id + 1}</h3><div className="vertical-rule"></div>
-            </div>
-            <div className="step-container">
-              <h4 className="step-title">{item[0]}</h4>
-              <p className='step-desc'>{item[1]}</p>
-            </div>
-          </li>
-        )
-      })
-
+//     const glassContent = glassSteps.map((item, id) => {
+//         return (
+//           <li key={id} className="item">
+//             <div className="step-number-container">
+//               <h3 className='step-number'>{id + 1}</h3><div className="vertical-rule"></div>
+//             </div>
+//             <div className="step-container">
+//               <h4 className="step-title">{item[0]}</h4>
+//               <p className='step-desc'>{item[1]}</p>
+//             </div>
+//           </li>
+//         )
+//       })
   return (
     <section className='service-content-container'>
       <Head>
@@ -49,13 +49,9 @@ export default function Addons() {
       <div className="cover" style={{backgroundImage: 'url(' + cover + ')' }}>
         <Image src={cover} alt="Vehicle after a Dirt-Less Wash with Glass Polishing addons" layout="fill" objectFit='cover' priority />
       </div>
-      <h1 className='servicepage-header'>Addons</h1>
+      <h1 className='servicepage-header'>Dirt-Less Detailing Addons</h1>
 
-      <div className='separator-container'>
-        <h2 className='service-title'>WATERSPOT/OVERSPRAY REMOVAL</h2>
-      </div>
-
-      <div className='main-content-container'>
+      {/* <div className='main-content-container'>
         <div className="process-container">
           <div>
             <p>This can be added onto any of our Exterior or Paint Correction Services when contacting us.</p>
@@ -76,15 +72,40 @@ export default function Addons() {
               <p className="pricing-price">$60/hour</p>
             </div>
           </div>
+        </div>
+      </div> */}
 
+      <div className="addon-content-container">
+        <div className="service-card">
+          <Image src={interior} alt="TODO: CHANGE THIS IMAGE" className='otherservices-image' loading='lazy' placeholder='blur' quality={1} />
+          <div className="otherservices-desc">
+            <a href="/services/FullIntEx" className='service-heading-link'>Waterspot/Overspray Removal</a>
+            <p className="secondary-text">Hourly rate: <span>$60/hour</span></p>
+            <p className="service-desc">Remove all overspray and waterspots we can.</p>
+            <p className="service-desc">• This can be added onto any of our Exterior Washes or any of our Paint Correction Services when contacting us.</p>
+
+            <div>
+              <a style={{marginBottom: 8}} href='/services/FullIntEx' className="readmore">Paint Correction Details {'>'}</a>
+              <a href='/services/DLWash' className="readmore">Dirt-Less Wash Details {'>'}</a>
+            </div>
+          </div>
+        </div>
+        <div className="service-card">
+          <Image src={interior} alt="TODO: CHANGE THIS IMAGE" className='otherservices-image' loading='lazy' placeholder='blur' quality={1} />
+          <div className="otherservices-desc">
+            <a href="/services/FullIntEx" className='service-heading-link'>Glass Polishing</a>
+            <p className="secondary-text">Hourly rate: <span>$70/hour</span></p>
+            <p className="service-desc">Remove all scratches, etching, and watermarks possible.</p>
+            <p className="service-desc">• This can be added onto any of our Paint Correction Services when contacting us.</p>
+
+            <div>
+              <a style={{marginBottom: 8}} href='/services/FullIntEx' className="readmore">Paint Correction Details {'>'}</a>
+            </div>
+          </div>
         </div>
       </div>
-
-      <div style={{marginTop: 75}} className='separator-container'>
-        <h2 className='service-title'>GLASS POLISHING</h2>
-      </div>
       
-      <div className='main-content-container'>
+      {/* <div className='main-content-container'>
         <div className="process-container">
           <div>
             <p>This can be added onto either of our Paint Correction Services when contacting us.</p>
@@ -109,7 +130,7 @@ export default function Addons() {
           <h4>Interested? Click "Get Started" in the Navigation to get in touch!</h4>
 
         </div>
-      </div>
+      </div> */}
     </section>
   )
 }
