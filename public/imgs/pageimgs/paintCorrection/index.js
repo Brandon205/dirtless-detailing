edcc6d6791cf261b552db9dd4457bba3176a6976
Brandon Black <brandonblack02@gmail.com>
2022-11-03 09:35:46 -0700

    Updated Paint Correction Gallery

--- public/imgs/pageimgs/paintCorrection/index.js
+++ public/imgs/pageimgs/paintCorrection/index.js
@@ -3,35 +3,21 @@ import Image from 'next/image';
 import Zoom from 'react-medium-image-zoom';
 import 'react-medium-image-zoom/dist/styles.css';
 
-import grayClose from './3456x4608a.webp';
-import blackClose from './3456x4608b.webp';
-import bmwHoodB from './3456x4608c.webp';
-import bmwHoodA from './3456x4608d.webp';
-import redRoofB from './3456x4608e.webp';
-import redHood1 from './3456x4608f.webp';
-import redHood2 from './3456x4608h.webp';
-import redHood3 from './3456x4608j.webp';
-import redSide1 from './3456x4608g.webp';
-import redSide2 from './3456x4608i.webp';
+function importAll(r) {
+    let images = {};
+    r.keys().map((item, index) => { images[item.replace('./', '')] = r(item); });
+    return images;
+  }
+  
+  let images = importAll(require.context('./', false, /\.(png|jpe?g|webp)$/));
 
-let images = [
-    grayClose,
-    blackClose,
-    bmwHoodA,
-    bmwHoodB,
-    redRoofB,
-    redHood1,
-    redHood2,
-    redHood3,
-    redSide1,
-    redSide2
-]
+images = Object.entries(images)
 
 let imagery = images.map((pic, id) => {
     return (
         <Zoom key={id}>
             <div className='gallery-image-div'>
-                <Image style={{borderRadius: 16}} alt={'paint correction example ' + id} src={images[id]} loading="lazy" placeholder='blur' />
+                <Image style={{borderRadius: 16}} alt={'paint correction example ' + id} src={images[id][1]} loading="lazy" placeholder='blur' />
             </div>
         </Zoom>
     )
