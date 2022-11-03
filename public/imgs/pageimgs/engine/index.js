576b7fd18defed85be9782a7bce9fd1805715816
Brandon Black <brandonblack02@gmail.com>
2022-11-03 08:17:55 -0700

    Updated images on engine gallery, + alg to import

--- public/imgs/pageimgs/engine/index.js
+++ public/imgs/pageimgs/engine/index.js
@@ -3,31 +3,23 @@ import Image from 'next/image';
 import Zoom from 'react-medium-image-zoom';
 import 'react-medium-image-zoom/dist/styles.css';
 
-import chevyEngine from './3456x4608a.webp';
-import engine2 from './3456x4608b.webp';
-import engine3 from './3456x4608c.webp';
-import engine4 from './3456x4608d.webp';
-import toyotaBay from './3456x4608e.webp';
-import mazdaBay from './3456x4608f.webp';
-import vanBay from './4032x3024a.webp';
+function importAll(r) {
+    let images = {};
+    r.keys().map((item, index) => { images[item.replace('./', '')] = r(item); });
+    return images;
+  }
+  
+  let images = importAll(require.context('./', false, /\.(png|jpe?g|webp)$/));
 
-let images = [
-    chevyEngine,
-    engine2,
-    engine3,
-    engine4,
-    toyotaBay,
-    mazdaBay,
-    vanBay
-]
+images = Object.entries(images)
 
 let imagery = images.map((pic, id) => {
     return (
         <Zoom key={id}>
-            <div className='gallery-image-div'>
-                <Image style={{borderRadius: 16}} alt={'engine example ' + id} src={images[id]} loading="lazy" placeholder='blur' />
-            </div>
-        </Zoom>
+           <div className='gallery-image-div'>
+               <Image style={{borderRadius: 16}} alt={'engine example ' + id} src={images[id][1]} loading="lazy" placeholder='blur' />
+           </div>
+       </Zoom>
     )
 })
 
