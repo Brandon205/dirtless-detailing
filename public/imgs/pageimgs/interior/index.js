bac19474fd4299279e1d37076e848d4824ad834a
Brandon Black <brandonblack02@gmail.com>
2022-11-03 09:30:54 -0700

    Interior Gallery Updates

--- public/imgs/pageimgs/interior/index.js
+++ public/imgs/pageimgs/interior/index.js
@@ -3,45 +3,21 @@ import Image from 'next/image';
 import Zoom from 'react-medium-image-zoom';
 import 'react-medium-image-zoom/dist/styles.css';
 
-import redToyota from './1080x720a.webp';
-import rearCupholders from './1080x1221a.webp';
-import driversSeat1 from './1080x1448a.webp';
-import trunk1 from './1080x1448b.webp';
-import centerCupholders from './1080x1448c.webp';
-import centerConsole from './1080x1920a.webp';
-import frontInt from './1080x1920b.webp';
-import rearSeats1 from './1080x1920c.webp';
-import rearSeats2 from './1080x1920d.webp';
-import driversSeat2 from './1080x1920e.webp';
-import floorMat1 from './1080x1920f.webp';
-import floorMat2 from './1080x1920g.webp';
-import passengerSeat1 from './3456x4608a.webp';
-import passengerSeat2 from './3456x4608b.webp';
-import driversSeat3 from './4608x3456a.webp';
+function importAll(r) {
+    let images = {};
+    r.keys().map((item, index) => { images[item.replace('./', '')] = r(item); });
+    return images;
+  }
+  
+  let images = importAll(require.context('./', false, /\.(png|jpe?g|webp)$/));
 
-let images = [
-    redToyota,
-    rearCupholders,
-    driversSeat1,
-    trunk1,
-    centerCupholders,
-    centerConsole,
-    frontInt,
-    rearSeats1,
-    rearSeats2,
-    driversSeat2,
-    floorMat1,
-    floorMat2,
-    passengerSeat1,
-    passengerSeat2,
-    driversSeat3
-]
+images = Object.entries(images)
 
 let imagery = images.map((pic, id) => {
     return (
         <Zoom key={id}>
             <div className='gallery-image-div'>
-                <Image style={{borderRadius: 16}} alt={'full interior example ' + id} src={images[id]} loading="lazy" placeholder='blur' />
+                <Image style={{borderRadius: 16}} alt={'full interior example ' + id} src={images[id][1]} loading="lazy" placeholder='blur' quality={1} />
             </div>
         </Zoom>
     )
