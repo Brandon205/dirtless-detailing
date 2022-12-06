44db717ded5e1842729da926fb070fcc842ebfea
Brandon Black <53108034+Brandon205@users.noreply.github.com>
2022-12-05 16:58:07 -0800

    More progress

--- pages/location/puyallup.jsx
+++ pages/location/puyallup.jsx
@@ -12,8 +12,12 @@ export default function puyallup() {
                 <meta property="og:description" content="Interior • Exterior • Ceramic Coating • Paint Correction. Check out our top auto detailing services for Puyallup Washington" />
                 <link rel="canonical" href="https://www.dirtlessdetailing.com/location/puyallup" />
             </Head>
-            <div className="location-cover">
+            <div className="cover location-cover">
                 <Image src={cover} alt="biohazard cleaning cover" layout="fill" objectFit='cover' placeholder='blur' priority />
+            </div>
+            <div className="servicepage-header location-header">
+                <h1>Mobile Detailing in Puyallup Washington</h1>
+                <a href="/Contact" className="quote-btn service-page-quote-btn">Get a Quote</a>
             </div>
             <main className="location-main">
                 <h1>Hello and stuff</h1>
