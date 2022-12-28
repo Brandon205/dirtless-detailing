1f2298f46c732ca37cd6b6452eef912c62c77a44
Brandon Black <53108034+Brandon205@users.noreply.github.com>
2022-12-27 17:42:35 -0800

    Schema fix part 1

--- utils/Schema.jsx
+++ utils/Schema.jsx
@@ -2,30 +2,30 @@ import React from 'react';
 
 export default function Schema() {
     return (
-        <script type="application/ld+json">{`
-        {
-          "@context":"https://schema.org/",
-          "@type":"LocalBusiness",
-          "currenciesAccepted": "USD",
-          "openingHours": ["Mo-Su 7:00-19:00"],
-          "paymentAccepted": "Cash, Credit/Debit Card, Facebook Pay",
-          "address": {
-            "@type": "PostalAddress",
-            "addressLocality": "Bonney Lake",
-            "addressRegion": "WA",
-            "postalCode": "99391",
-            "streetAddress": "9305 205th Ave E"
-          },
-          "email": "brenden@dirtlessdetailing.com",
-          "telephone": "(253) 252-9758",
-          "makesOffer": {
-            "@type": "Offer",
-            "itemOffered": {
-                "@type": "Service",
-                "name": "Full Interior Detail"
+        <script type="application/ld+json" dangerouslySetInnerHTML={
+          {
+            "@context":"https://schema.org/",
+            "@type":"LocalBusiness",
+            "currenciesAccepted": "USD",
+            "openingHours": ["Mo-Su 7:00-19:00"],
+            "paymentAccepted": "Cash, Credit/Debit Card, Facebook Pay",
+            "address": {
+              "@type": "PostalAddress",
+              "addressLocality": "Bonney Lake",
+              "addressRegion": "WA",
+              "postalCode": "99391",
+              "streetAddress": "9305 205th Ave E"
+            },
+            "email": "brenden@dirtlessdetailing.com",
+            "telephone": "(253) 252-9758",
+            "makesOffer": {
+              "@type": "Offer",
+              "itemOffered": {
+                  "@type": "Service",
+                  "name": "Full Interior Detail"
+              }
             }
           }
-        }
-        `}</script>
+        } />
     )
 }
