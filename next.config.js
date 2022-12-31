019360ba26f477628e3eab618e2e96080b1121fd
Brandon Black <brandonblack02@gmail.com>
2022-12-31 11:42:02 -0800

    Removed protocols and added cloudinary to next

--- next.config.js
+++ next.config.js
@@ -2,41 +2,15 @@ const withPWA = require('next-pwa')({
     dest: 'public',
     register: true
   })
-
-  const ContentSecurityPolicy = `
-    default-src 'self';
-    script-src 'self';
-    child-src example.com;
-    style-src 'self' example.com;
-    font-src 'self';  
-  `
-
-  const securityHeaders = [
-    {
-      key: 'X-Content-Type-Options',
-      value: 'nosniff'
-    },
-    {
-      key: 'X-Frame-Options',
-      value: 'SAMEORIGIN'
-    },
-    {
-      key: 'Referrer-Policy',
-      value: 'strict-origin-when-cross-origin'
-    },
-    {
-      key: 'Content-Security-Policy',
-      value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim()
-    }
-  ]
   
   module.exports = withPWA({
-    async headers() {
-      return [
+    images: {
+      remotePatterns: [
         {
-          source: '/:path*',
-          headers: securityHeaders,
+          protocol: 'https',
+          hostname: 'res.cloudinary.com',
+          port: ''
         },
-      ]
+      ],
     },
   })
