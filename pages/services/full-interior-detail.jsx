ead4e984969b2954dcd97a58994428546d51689c
Brandon Black <53108034+Brandon205@users.noreply.github.com>
2023-01-06 22:15:16 -0800

    Redid aside container behavior on full interior

--- pages/services/full-interior-detail.jsx
+++ pages/services/full-interior-detail.jsx
@@ -97,8 +97,7 @@ export default function FullIntEx() {
               <h2 className='text-large'>Professional Interior Detailing</h2>
               <div className="text-left">
                 <p>The Full Interior Cleaning package is meant to get your interior back to the best condition possible. Your seats, floor mats, plastics, windows, and carpet will all be thoroughly cleaned, detailed, and shampooed/extracted. </p>
-                <p>If it can be cleaned, we'll clean it. Does that mean every little stain will be removed? Unfortunately, that isn't the case. Based on the type of stain, how long it's been sitting, and the material, many stains won't come out 100%.</p>
-                <p>Of course we'll still do our best to reduce the appearance of it.</p>
+                <p>If it can be cleaned, we'll clean it. Does that mean every little stain will be removed? Unfortunately, that isn't the case. Based on the type of stain, how long it's been sitting, and the material, many stains won't come out 100%. Of course we'll still do our best to reduce the appearance of it.</p>
                 <p>Please don't think your interior is “too dirty”. We have the necessary tools and products to get the job done. We have cleaned hundreds of interiors, of all conditions. It's not easy to surprise us.</p>
                 <p>Make it a <a href='/services/interior-exterior-detailing-combo' className='aside-link'>Combo</a> deal by booking with a Premium Dirt-Less Wash, and get a free <a href='/services/add-ons#engine' className='aside-link'>Engine Bay</a> cleaning on the house!</p>
               </div>
@@ -112,6 +111,45 @@ export default function FullIntEx() {
             </ol>
             <p style={{ marginTop: 0 }}>*Headliners offered as a separate add-on</p>
             <p><span className='special-package'>*Extraction</span> Package Only</p>
+
+            <br className='extra-space' />
+
+            <div>
+              <h3 className='pricing-title'>Our Full Interior Cleaning Pricing</h3>
+              <strong className='pricing-subtext'>*Final pricing may vary based on your vehicle condition and size.</strong>
+              <div className="pricing-container">
+                <div className="pricing-card">
+                  <strong className='size' style={{ fontSize: 1.4 + 'em', fontWeight: 'bold' }}>Small</strong>
+                  <strong className='size'>Without Extraction</strong>
+                  <p className="pricing-price pricing-border">$200</p>
+                  <strong className='size'>With Extraction</strong>
+                  <p className="pricing-price pricing-border">$250</p>
+                  <p className="example-vehicle pricing-border">2-Door Coupes</p>
+                  <p className="example-vehicle pricing-border">Single/Extended Cabs</p>
+                  <a href="/Contact" className="pricing-quote-btn">Contact Us</a>
+                </div>
+                <div className="pricing-card">
+                  <strong className='size' style={{ fontSize: 1.4 + 'em', fontWeight: 'bold' }}>Medium</strong>
+                  <strong className='size'>Without Extraction</strong>
+                  <p className="pricing-price pricing-border">$250</p>
+                  <strong className='size'>With Extraction</strong>
+                  <p className="pricing-price pricing-border">$300</p>
+                  <p className="example-vehicle pricing-border">4 Door Vehicles</p>
+                  <p className="example-vehicle pricing-border">Smaller SUV's</p>
+                  <a href="/Contact" className="pricing-quote-btn">Contact Us</a>
+                </div>
+                <div className="pricing-card">
+                  <strong className='size' style={{ fontSize: 1.4 + 'em', fontWeight: 'bold' }}>Large</strong>
+                  <strong className='size'>Without Extraction</strong>
+                  <p className="pricing-price pricing-border">$300</p>
+                  <strong className='size'>With Extraction</strong>
+                  <p className="pricing-price pricing-border">$350</p>
+                  <p className="example-vehicle pricing-border">Larger Trucks</p>
+                  <p className="example-vehicle pricing-border">Extra Large SUV's</p>
+                  <a href="/Contact" className="pricing-quote-btn">Contact Us</a>
+                </div>
+              </div>
+            </div>
           </div>
 
           <div className='aside-container'>
@@ -125,82 +163,44 @@ export default function FullIntEx() {
               </div>
             </div>
 
-            <div className='aside-container'>
-              <div className='service-aside addons-aside'>
-                <p className='extra-addons text-large'>Extra Add-ons</p>
-                <hr className="contact-border" style={{ marginBottom: 16 }} />
-                <div className="aside-links">
-                  <div className="addon-container">
-                    <p className='addon-title'><a href="/services/interior-ceramic-coating" className='text-blue'>Fabric Coating</a></p>
-                    <p className='m-0'><span className="bold special-package">$75</span></p>
-                    <p className="addon-subtext">The best protection for the fabric inside of your vehicle.</p>
-                  </div>
-                  <hr className='aside-border' />
-                  <div className="addon-container">
-                    <p className='addon-title'><a href="/services/interior-ceramic-coating" className='text-blue'>Leather Coating</a></p>
-                    <p className='m-0'><span className="bold special-package">$150</span></p>
-                    <p className="addon-subtext">The longest lasting protection for your vehicles leather.</p>
-                  </div>
-                  <hr className='aside-border' />
-                  <div className="addon-container">
-                    <p className='addon-title'><a href="/services/add-ons#headliners" className='text-blue'>Headliners</a></p>
-                    <p className='m-0'><span className="bold special-package">$40-$80</span></p>
-                    <p className="addon-subtext">Clean your vehicles headliners.</p>
-                  </div>
-                  <hr className='aside-border' />
-                  <div className="addon-container">
-                    <p className='addon-title'><a href="/services/add-ons#ozone" className='text-blue'>Ozone Treatment</a></p>
-                    <p className='m-0'><span className="bold special-package">$100</span></p>
-                    <p className="addon-subtext">Treats your vehicle to get rid of any viruses, bacteria, and odors.</p>
-                  </div>
+            <div className="service-aside addons-aside">
+              <h3 className="text-white">Get a <span className='special-package'>FREE</span> Engine cleaning!</h3>
+              <p className="subtext">Check out our Current <a className='aside-link' href="/services/interior-exterior-detailing-combo">Combo</a> deal that combines this Premium Dirt-Less Wash, our <a className='aside-link' href="/services/full-interior-detail">Full Interior Cleaning</a>, and gives you an Engine Bay Cleaning totally on us!  </p>
+            </div>
+
+            <div className='service-aside addons-aside'>
+              <p className='extra-addons text-large'>Extra Add-ons</p>
+              <hr className="contact-border" style={{ marginBottom: 16 }} />
+              <div className="aside-links">
+                <div className="addon-container">
+                  <p className='addon-title'><a href="/services/interior-ceramic-coating" className='text-blue'>Fabric Coating</a></p>
+                  <p className='m-0'><span className="bold special-package">$75</span></p>
+                  <p className="addon-subtext">The best protection for the fabric inside of your vehicle.</p>
+                </div>
+                <hr className='aside-border' />
+                <div className="addon-container">
+                  <p className='addon-title'><a href="/services/interior-ceramic-coating" className='text-blue'>Leather Coating</a></p>
+                  <p className='m-0'><span className="bold special-package">$150</span></p>
+                  <p className="addon-subtext">The longest lasting protection for your vehicles leather.</p>
+                </div>
+                <hr className='aside-border' />
+                <div className="addon-container">
+                  <p className='addon-title'><a href="/services/add-ons#headliners" className='text-blue'>Headliners</a></p>
+                  <p className='m-0'><span className="bold special-package">$40-$80</span></p>
+                  <p className="addon-subtext">Clean your vehicles headliners.</p>
+                </div>
+                <hr className='aside-border' />
+                <div className="addon-container">
+                  <p className='addon-title'><a href="/services/add-ons#ozone" className='text-blue'>Ozone Treatment</a></p>
+                  <p className='m-0'><span className="bold special-package">$100</span></p>
+                  <p className="addon-subtext">Treats your vehicle to get rid of any viruses, bacteria, and odors.</p>
                 </div>
               </div>
             </div>
-
           </div>
+
         </div>
 
-        <div className="combo-container">
-          <h3 className='text-white'>Get a <span className='special-package'>FREE</span> Engine cleaning!</h3>
-          <p className="subtext">Check out our Current <a className='aside-link' href="/services/interior-exterior-detailing-combo">Combo</a> deal that combines this Full Interior Cleaning, our <a className='aside-link' href="/services/premium-dirtless-wash">Premium Dirt-Less Wash</a>, and gives you an Engine Bay cleaning totally on us!  </p>
-        </div>
-
-        <div>
-          <h3 className='pricing-title'>Our Full Interior Cleaning Pricing</h3>
-          <strong className='pricing-subtext'>*Final pricing may vary based on your vehicle condition and size.</strong>
-          <div className="pricing-container">
-            <div className="pricing-card">
-              <strong className='size' style={{ fontSize: 1.4 + 'em', fontWeight: 'bold' }}>Small</strong>
-              <strong className='size'>Without Extraction</strong>
-              <p className="pricing-price pricing-border">$200</p>
-              <strong className='size'>With Extraction</strong>
-              <p className="pricing-price pricing-border">$250</p>
-              <p className="example-vehicle pricing-border">2-Door Coupes</p>
-              <p className="example-vehicle pricing-border">Single/Extended Cabs</p>
-              <a href="/Contact" className="pricing-quote-btn">Contact Us</a>
-            </div>
-            <div className="pricing-card">
-              <strong className='size' style={{ fontSize: 1.4 + 'em', fontWeight: 'bold' }}>Medium</strong>
-              <strong className='size'>Without Extraction</strong>
-              <p className="pricing-price pricing-border">$250</p>
-              <strong className='size'>With Extraction</strong>
-              <p className="pricing-price pricing-border">$300</p>
-              <p className="example-vehicle pricing-border">4 Door Vehicles</p>
-              <p className="example-vehicle pricing-border">Smaller SUV's</p>
-              <a href="/Contact" className="pricing-quote-btn">Contact Us</a>
-            </div>
-            <div className="pricing-card">
-              <strong className='size' style={{ fontSize: 1.4 + 'em', fontWeight: 'bold' }}>Large</strong>
-              <strong className='size'>Without Extraction</strong>
-              <p className="pricing-price pricing-border">$300</p>
-              <strong className='size'>With Extraction</strong>
-              <p className="pricing-price pricing-border">$350</p>
-              <p className="example-vehicle pricing-border">Larger Trucks</p>
-              <p className="example-vehicle pricing-border">Extra Large SUV's</p>
-              <a href="/Contact" className="pricing-quote-btn">Contact Us</a>
-            </div>
-          </div>
-        </div>
       </div>
     </section>
   )
