a579807f743d386ab6d8d66698100b9cf01e221f
Brandon Black <brandonblack02@gmail.com>
2023-01-23 17:58:51 -0800

    Biohazard page update

--- pages/services/biohazard-cleaning.jsx
+++ pages/services/biohazard-cleaning.jsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import Zoom from 'react-medium-image-zoom';
 import Image from 'next/image';
 import Head from 'next/head';
+import ReactPlayer from 'react-player/lazy'; 
+import Zoom from 'react-medium-image-zoom';
 import 'react-medium-image-zoom/dist/styles.css';
 
 import cover from '../../public/imgs/BioCover.webp';
@@ -84,7 +85,7 @@ export default function Bio() {
       </div>
       <div className="servicepage-header">
         <h1>Biohazard Cleaning</h1>
-        <h2 className='page-subheading'>Deeper stains and spills leaving unwanted odors? Look no further than our Biohazard cleaning.</h2>
+        <h2 className='page-subheading'>Mold, odors, or other mildly hazardous spills in your vehicle? Look no further than our Biohazard cleaning.</h2>
         <a href="/Contact" className="quote-btn service-page-quote-btn">Get a Quote</a>
         <p className='inline text-small'>
           Call/Text: <a href="tel:2532529758" className="contact-link footer-contact-link" target="_blank" rel="noopener">(253) 252-9758</a>
@@ -95,11 +96,16 @@ export default function Bio() {
         <div className='process-container'>
           <div>
             <main className="service-main">
-              <h2 className='text-large'>Professional Biohazard Cleaning</h2>
-              <div className="text-left">
-                <p>No one likes surprises. Especially not a smelly, moldy car.  Sometimes we get called to clean mold, urine, or even a busted gallon of spoiled milk. All of these are considered biohazard cleaning.</p>
-                <p>The sooner we can get to it, the easier it is to clean up. Depending on where the accident took place, how much fluid came out, and how long ago the accident happened will determine what we can do.</p>
-                <p>Generally speaking, if there is a smell lingering in the interior due to an accident, the smell should be gone once it is cleaned up.  We also have a state of the art ozone machine to blast away any remaining odors and sanitize the air.</p>
+              <div className="service-explain">
+                <div className="text-left">
+                  <h2 className='text-large'>Professional Biohazard Cleaning</h2>
+                  <p>No one likes surprises. Especially not a smelly, moldy car.  Sometimes we get called to clean mold, urine, or even a busted gallon of spoiled milk. All of these are considered biohazard cleaning.</p>
+                  <p>The sooner we can get to it, the easier it is to clean up. Depending on where the accident took place, how much fluid came out, and how long ago the accident happened will determine what we can do.</p>
+                  <p>Generally speaking, if there is a smell lingering in the interior due to an accident, the smell should be gone once it is cleaned up.  We also have a state of the art ozone machine to blast away any remaining odors and sanitize the air.</p>
+                </div>
+                <div className="service-image">
+                  <ReactPlayer width={280} height={487} url='https://res.cloudinary.com/brandon205/video/upload/v1674493881/Dirt-Less%20Detailing/Videos/received_1068200607393416_2_ln04go.mp4' loop muted playing />
+                </div>
               </div>
 
               <br className='extra-space' />
@@ -148,29 +154,6 @@ export default function Bio() {
 
           <br className='extra-space' />
 
-          <div className='aside-container'>
-            <div className='service-aside'>
-              <p className='extra-addons text-large'>Other Interior Pages</p>
-              <hr className="contact-border" style={{ marginBottom: 16 }} />
-              <div className="aside-links">
-                <a href="/services/full-interior-detail" style={{ color: href.endsWith('full-interior-detail') ? 'dodgerblue' : 'white' }} className="service-link">{href.endsWith('full-interior-detail') ? '> ' : ''} Full Interior Detail</a><br />
-                <a href="#" style={{ color: href.endsWith('biohazard-cleaning') ? 'dodgerblue' : 'white' }} className="service-link">{href.endsWith('biohazard-cleaning') ? '> ' : ''} Biohazard Cleaning</a><br />
-                <a href="/gallery/interior-detail-gallery" className="service-link">Full Interior Gallery</a>
-              </div>
-            </div>
-            {/* <div className='service-aside addons-aside'>
-              <p className='extra-addons text-large'>Extra Add-ons</p>
-              <hr className="contact-border" style={{ marginBottom: 16 }} />
-              <div className="aside-links">
-                <div className="addon-container">
-                  <p className='addon-title'><a href="/services/add-ons#ozone" className='text-blue'>Ozone Treatment</a></p>
-                  <p className='m-0'><span className="bold special-package">$100</span></p>
-                  <p className="addon-subtext">Treats your vehicle to get rid of any viruses, bacteria, and odors.</p>
-                </div>
-              </div>
-            </div> */}
-
-          </div>
         </div>
 
       </div>
