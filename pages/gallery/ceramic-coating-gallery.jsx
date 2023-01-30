9c312a00724888ee1b482a017218c02ac0a9a290
Brandon Black <brandonblack02@gmail.com>
2023-01-30 11:15:11 -0800

    Ceramic and exterior gallery images hosted

--- pages/gallery/ceramic-coating-gallery.jsx
+++ pages/gallery/ceramic-coating-gallery.jsx
@@ -1,10 +1,7 @@
 import React from 'react';
-import Image from 'next/image';
 import Head from 'next/head';
 import Metatags from '../../utils/Metatags';
 import { imagery } from '../../public/imgs/pageimgs/ceramic/index';
-
-import cover from '../../public/imgs/pageimgs/paintCorrection/IMG_20210526_055946.jpg';
 
 export default function CoatingGallery() {
   return (
@@ -14,13 +11,11 @@ export default function CoatingGallery() {
         <Metatags
           title='Dirt-Less Detailing | Ceramic Coating Gallery'
           description='A gallery of photos that we have taken showing off some of the vehicles that we have coated and protected!'
-          image={cover.src}
+          image='https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/8c1574b9-7517-4c50-6b36-133352ab0a00/public'
           canonical='/gallery/ceramic-coating-gallery'
         />
       </Head>
-      <div className="cover">
-        <Image src={cover} alt="Vehicle after a Ceramic Coating" layout="fill" objectFit='cover' priority />
-      </div>
+      <div className="cover" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/8c1574b9-7517-4c50-6b36-133352ab0a00/public')`}}></div>
       <div className="servicepage-heading">
         <h1>Ceramic Coating Gallery</h1>
         <p>Check out the gallery below to see the results of a freshly applied IGL Ceramic Coating. Take a look at our <a href="/services/interior-ceramic-coating" className='aside-link' >Interior Coatings</a> page or our <a href="/detailing/ceramic" className='aside-link' >Ceramic Coating</a> page to learn more about these services.</p>
@@ -30,9 +25,33 @@ export default function CoatingGallery() {
           Call/Text: <a href="tel:2532529758" className="contact-link footer-contact-link" target="_blank" rel="noopener">(253) 252-9758</a>
         </p>
       </div>
-
-      <div className='gallery-container'>
-        {imagery}
+      <div className="photo-grid">
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/2a8430f3-90c0-4f56-1fff-88fcd3b29600/public')`}}></div>
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/318b00ed-4a02-4f43-7fdb-1d407d7b0100/public')`}}></div>
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/3c484dfb-5247-428e-1735-1b31f3efd600/public')`}}></div>
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/a4a318e1-fd6c-4f89-7539-55b66a2d6200/public')`}}></div>
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/d3d11e10-2cbe-4454-9a36-fbc43dea7800/public')`}}></div>
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/43a02c30-4982-435d-7f91-6d81fd11a000/public')`}}></div>
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/6feedf70-363e-4a11-8e18-185c9d984600/public')`}}></div>
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/fdaa6115-35ea-45f4-e7f2-acec67e40a00/public')`}}></div>
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/fdce3625-66ac-4ea8-68e5-9f2e9e94c500/public')`}}></div>
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/e30050eb-d6a4-45c9-bb0c-c5070cd86700/public')`}}></div>
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/171029c2-9bcd-46da-fa7d-b45f5e6e9800/public')`}}></div>
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/74e3cbf9-f5d7-458f-14c3-5367bc903e00/public')`}}></div>
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/ab68bfcc-17a0-4468-539b-fded52edc500/public')`}}></div>
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/48ede291-7184-49d8-d6b4-cee59b767c00/public')`}}></div>
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/92eba802-0109-4a56-f3ae-cd929116ea00/public')`}}></div>
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/611a9397-5a44-416d-03f6-491aae2bde00/public')`}}></div>
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/c8855b02-88e3-47c6-f8ab-835cead38f00/public')`}}></div>
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/eef9854b-8f53-4f66-fa7f-d7c97fc71e00/public')`}}></div>
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/dd8a4ba8-beaa-4361-cbc9-f50e9bda3f00/public')`}}></div>
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/e0f2ecbc-7591-4ec8-534c-28b3261ec700/public')`}}></div>
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/0debc625-8006-4be7-1ae8-4fe3d43ae700/public')`}}></div>
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/8c1574b9-7517-4c50-6b36-133352ab0a00/public')`}}></div>
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/5b0798e5-2648-4845-168b-98e7ff013100/public')`}}></div>
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/a5260bdd-11e9-4e31-74ec-c1d75787e000/public')`}}></div>
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/0b441cde-1d0f-45d9-665f-e44bfce0d800/public')`}}></div>
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/2a196c77-cb1a-42f9-479b-8fdbe3c14500/public')`}}></div>
       </div>
       <h2 style={{ color: 'white' }}>Check out our <a href="/services/interior-ceramic-coating" className='aside-link' >Interior Coatings</a> page or our <a href="/detailing/ceramic" className='aside-link' >Ceramic Coating</a> page for more details!</h2>
     </section>
--- pages/gallery/dirtless-wash-gallery.jsx
+++ pages/gallery/dirtless-wash-gallery.jsx
@@ -1,10 +1,6 @@
 import React from 'react';
-import Image from 'next/image';
 import Head from 'next/head';
 import Metatags from '../../utils/Metatags';
-import { imagery } from '../../public/imgs/pageimgs/exterior/index';
-
-import cover from '../../public/imgs/pageimgs/exterior/IMG_20210429_202336.jpg';
 
 export default function PaintCorrectionGallery() {
 
@@ -15,13 +11,11 @@ export default function PaintCorrectionGallery() {
         <Metatags
           title='Car Wash Gallery | Dirt-Less Detailing'
           description='Take a look at our showcase of our exterior wash services to get a preview of what we are capable of doing to your car, no matter the current condition!'
-          image={cover.src}
+          image='https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/a33a454b-ac45-4972-91f7-3d80eda98900/public'
           canonical='/gallery/dirtless-wash-gallery'
         />
       </Head>
-      <div className="cover">
-        <Image src={cover} alt="Vehicle after a Dirt-Less Wash" layout="fill" objectFit='cover' priority />
-      </div>
+      <div className="cover" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/a33a454b-ac45-4972-91f7-3d80eda98900/public')`}}></div>
       <div className="servicepage-heading">
         <h1>Dirt-Less Wash Gallery</h1>
         <p>This is a showcase of our Dirt-Less Wash service. Take a look at our <a href="/detailing/car-washes" className='aside-link'>Car Washes</a> page for more info on the 2 exterior wash services that we offer.</p>
@@ -31,8 +25,99 @@ export default function PaintCorrectionGallery() {
           Call/Text: <a href="tel:2532529758" className="contact-link footer-contact-link" target="_blank" rel="noopener">(253) 252-9758</a>
         </p>
       </div>
-      <div className='gallery-container'>
-        {imagery}
+      <div className="photo-grid">
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/abad4059-5853-4e69-d9ca-bd3717588a00/public')`}}></div>
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/cfe10ac7-b437-47a5-4e92-8e2c93e69200/public')`}}></div>
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/82ea1adf-76c8-4ac0-e292-3713c60dd600/public')`}}></div>
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/e7077cc3-0548-4eea-4846-c76ee44d2200/public')`}}></div>
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/2b512f99-95ee-41b6-e409-f263be54aa00/public')`}}></div>
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/8e720a1d-bebe-4512-9039-155931253200/public')`}}></div>
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/926e9a72-211b-4d26-e627-f76660e35100/public')`}}></div>
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/19226e28-a8ae-45a7-4743-1dd8b11c4900/public')`}}></div>
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/8da33934-bc23-47f5-9b0a-2b5264566600/public')`}}></div>
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/be00dc2c-44a8-432d-2ab8-7ef901b08100/public')`}}></div>
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/1388f951-7a9b-49da-526b-39d8ccdbbe00/public')`}}></div>
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/6667c959-e20c-4ec4-03a8-c9219eb78c00/public')`}}></div>
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/8d0fdc90-2463-4bce-ca3d-3ebb0730ef00/public')`}}></div>
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/de7d3d63-8a46-4909-181f-7e3560be1d00/public')`}}></div>
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/e35e3b8c-213d-456c-e8e6-1dbf2e2a0a00/public')`}}></div>
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/25415411-9612-4dcc-0b93-9d577a42f400/public')`}}></div>
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/52d87171-14af-476c-503a-1b2e7b5dc300/public')`}}></div>
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/3c5ebd25-6830-4a01-7b42-642f13790600/public')`}}></div>
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/02bfdebc-f1e8-48a2-e543-6a3236298600/public')`}}></div>
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/3db5b356-a6ac-4e19-6ba8-c094ba117300/public')`}}></div>
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/3945e495-8e00-40cf-9b92-8519b6c67f00/public')`}}></div>
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/153efb98-6c1d-4671-c068-24b46fc7af00/public')`}}></div>
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/c274c919-595c-4283-2d10-c5a47e569700/public')`}}></div>
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/b1136594-a8fc-48a2-9c6f-6b94d123c000/public')`}}></div>
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/741cf4dc-9ff4-431b-faa5-0ee043959600/public')`}}></div>
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/a33a454b-ac45-4972-91f7-3d80eda98900/public')`}}></div>
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/12142ae1-d38a-49c6-0a25-18353e3c0600/public')`}}></div>
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/aa8e36e7-b22d-45d7-9add-b4ccccb21300/public')`}}></div>
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/b380fa7c-bc37-4936-3090-e699be40c300/public')`}}></div>
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/d8b99543-bf33-46c4-9795-a1ecb55c0400/public')`}}></div>
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/ab71ce90-9668-4ae9-30fb-f12a9d7a5a00/public')`}}></div>
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/ecf3450f-32ca-4b8b-875f-af31496c1000/public')`}}></div>
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/abfa2236-a08d-4f22-3766-53b148cb9d00/public')`}}></div>
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/fa4b1452-9c70-4f81-779e-d7c010341500/public')`}}></div>
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/5c7fc46a-ada8-4ee1-a95f-3faaf47ded00/public')`}}></div>
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/67276280-d496-4545-b796-b73cff07d500/public')`}}></div>
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/edc1159d-4a1a-4309-2caa-57dc676c7d00/public')`}}></div>
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/fe0c1c1d-4c2b-4bd4-d959-e5d325d87b00/public')`}}></div>
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/4d96d540-305a-4b82-ac36-a45a55889300/public')`}}></div>
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/607e611c-ff17-4626-8e4a-2a2d18514200/public')`}}></div>
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/ccc88fac-e8ba-4768-b452-fa1ca2227800/public')`}}></div>
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/5794cb73-4054-4a66-30bc-bb31ea833500/public')`}}></div>
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/2b4f72db-a0cc-4f91-ed15-66d8a1b4c400/public')`}}></div>
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/942172f8-bb7a-4858-9165-fc94b7dbfc00/public')`}}></div>
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/b5859464-3c0e-4fcf-8332-17155eb86d00/public')`}}></div>
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/52f174e6-b845-457b-9249-ca2d53cc9900/public')`}}></div>
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/e4833336-a1de-4443-2603-21721fed2200/public')`}}></div>
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/404357ce-1fb8-4de6-4c00-7c74beaac800/public')`}}></div>
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/de70cff5-0f74-4839-d319-a9d805158c00/public')`}}></div>
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/cc5ed73c-7c7c-4bbe-73a5-64b687e40b00/public')`}}></div>
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/0a118c00-eb44-4de0-e584-46e07c17a800/public')`}}></div>
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/fbacbf21-f27a-4389-de9d-aa470df43000/public')`}}></div>
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/147c3ec4-2b37-47ed-d8f2-dacd223a3d00/public')`}}></div>
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/dfcc9990-ee88-465d-de08-97f94b37b000/public')`}}></div>
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/89cde8ad-b8a8-4d15-5a65-786ad9772d00/public')`}}></div>
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/14c28fdc-5ffa-4b61-696f-31e3afde9400/public')`}}></div>
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/d4f1595e-a058-4e98-4644-4eff8ce08b00/public')`}}></div>
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/ca33e36e-1ee2-454e-84a3-7d53c8397a00/public')`}}></div>
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/766b6381-60a8-403a-90f8-c781816dd100/public')`}}></div>
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/968f206e-69c5-4caf-0516-066635f49300/public')`}}></div>
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/29eb684e-a1c5-4590-6aa1-e3bd5f7f5d00/public')`}}></div>
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/f2fc5016-bb6c-4051-69e9-d533d51b8600/public')`}}></div>
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/b94c976f-1d15-43bb-2370-2a11cae79d00/public')`}}></div>
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/57c19bd3-e805-4718-1058-3b06d9039000/public')`}}></div>
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/abd4b7ca-ee1b-493e-4ae0-2c4e86f94c00/public')`}}></div>
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/abd4b7ca-ee1b-493e-4ae0-2c4e86f94c00/public')`}}></div>
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/aefa9e3f-cdda-4ff7-3aa5-8f39c8035500/public')`}}></div>
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/8eae56ed-af04-4346-0486-ab8b39551a00/public')`}}></div>
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/275209f1-197a-41be-c64c-d94817a44000/public')`}}></div>
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/3f912781-8f95-4a53-13b1-4fe7ac4e3800/public')`}}></div>
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/65eea438-142c-4422-24fe-16ec7eac3700/public')`}}></div>
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/fb257dd1-ee29-4721-2d59-ad017238cd00/public')`}}></div>
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/40124dfe-3a15-46bb-4017-4abbacf6b000/public')`}}></div>
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/907e80c4-04bf-4ce8-92ee-9b2ab22d4000/public')`}}></div>
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/dcb8ea42-e32b-49cf-2fc9-3529cb75af00/public')`}}></div>
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/df5ba5d7-e0e6-4868-ec67-6fcdcfbf3900/public')`}}></div>
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/7352d265-8a1d-4b7d-32ac-15358eb9b800/public')`}}></div>
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/776ba643-08c5-44d3-612e-b4154533be00/public')`}}></div>
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/6bb810de-45bd-40ea-bd8f-8c72dff61e00/public')`}}></div>
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/c1bf41d1-5f60-454f-ecd2-1887f2172400/public')`}}></div>
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/8bb70c75-e479-4df2-19a4-f0b673fb8f00/public')`}}></div>
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/96138506-01f7-40ce-dede-e6dc8143e800/public')`}}></div>
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/eb7ab094-6509-4f2e-2166-a7a143815500/public')`}}></div>
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/2a21bdf2-eb2c-4e59-7949-297aac1f2c00/public')`}}></div>
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/a63e4d0f-d6bb-4815-ded0-d802a089d500/public')`}}></div>
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/c0bdc93c-37a1-41f8-60cc-4eba27e9a100/public')`}}></div>
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/079071c2-451b-4dab-9603-33d3620bd300/public')`}}></div>
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/eb8099ef-8be3-4be2-77bc-ec6b65a7fc00/public')`}}></div>
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/31265123-5fa7-4f81-af49-d224e86beb00/public')`}}></div>
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/5e5a1366-292c-4c52-3983-98f2e0b2d600/public')`}}></div>
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/8f189b34-d8a0-41de-f607-42ffc20e8600/public')`}}></div>
+        <div className="card" style={{backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/b097bffc-4ed9-48c6-63ee-f1fa12f7bf00/public')`}}></div>
       </div>
       <h2 style={{ color: 'white' }}>Check out our <a href="/services/dirtless-wash" className='aside-link'>Dirt-Less Wash</a> page for more details!</h2>
     </section>
