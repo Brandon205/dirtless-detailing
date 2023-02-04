c0925e924ee567be887cfb3708ce5101cc5583f5
Brandon Black <53108034+Brandon205@users.noreply.github.com>
2023-02-03 16:46:38 -0800

    Started funnel work

---
+++ pages/funnel.jsx
@@ -0,0 +1,9 @@
+import styles from './Funnel.module.css';
+
+export default function funnel() {
+    return (
+        <div style={{height: 100 + 'vh', paddingTop: 5 + 'rem'}} className='bg-contact'>
+            <h1 className={styles.head1}>Funnel page here.</h1>
+        </div>
+    )
+}
