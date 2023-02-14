9c098144977d7ac0a2531a46f63279fe626d1c44
Brandon Black <53108034+Brandon205@users.noreply.github.com>
2023-02-14 08:18:17 -0800

    Faq Styling changes

--- pages/funnel/faq.jsx
+++ pages/funnel/faq.jsx
@@ -15,28 +15,28 @@ export default function Faq() {
         <div className={styles.faqContainer}>
             <ul className={styles.faqUl}>
                 <li className={styles.liContainer}>
-                    <h4 onClick={() => setQ1(!q1)} className={styles.faqQ}><BsQuestionCircleFill className={styles.faqIcon} /> How long does it last? {q1 ? <RxCaretUp className={styles.faqCaret} /> : <RxCaretDown className={styles.faqCaret} />}</h4>
-                    <p style={{height: q1 ? 'auto' : 0, opacity: q1 ? 1 : 0}} className={styles.faqP}>The paint coating should last 5 years, 50,000 miles, or a Lifetime<span className='special-package'>*</span> with our warranty. The coating on any trim, glass, lights, or wheels will last 1 year approximately.</p>
-                </li>
-
-                <li className={styles.liContainer}>
-                    <h4 onClick={() => setQ2(!q2)} className={styles.faqQ}><BsQuestionCircleFill className={styles.faqIcon} /> Can I still go through car washes? {q2 ? <RxCaretUp className={styles.faqCaret} /> : <RxCaretDown className={styles.faqCaret} />}</h4>
-                    <p style={{height: q2 ? 'auto' : 0, opacity: q2 ? 1 : 0}} className={styles.faqP}>If you want to keep the ceramic coat in good condition and have it last as long as possible, you won't want to go through any car washes even the touch-less ones.</p>
-                </li>
-
-                <li className={styles.liContainer}>
                     <h4 onClick={() => setQ3(!q3)} className={styles.faqQ}><BsQuestionCircleFill className={styles.faqIcon} /> How should I wash my car then? {q3 ? <RxCaretUp className={styles.faqCaret} /> : <RxCaretDown className={styles.faqCaret} />}</h4>
-                    <p style={{height: q3 ? 'auto' : 0, opacity: q3 ? 1 : 0}} className={styles.faqP}>For washing your coated vehicle you have a couple of options. We recommend scheduling monthly maintenance through us especially if you get our Protect+ Package. Or we can give you a run down on our techniques and recommend you some products to use to prolong the life of your ceramic coating!</p>
+                    <p style={{maxHeight: q3 ? '100px' : 0, opacity: q3 ? 1 : 0}} className={styles.faqP}>For washing your coated vehicle you have a couple of options. We recommend scheduling monthly maintenance through us especially if you get our Protect+ Package. Or we can give you a run down on our techniques and recommend you some products to use to prolong the life of your ceramic coating!</p>
+                </li>
+
+                <li className={styles.liContainer}>
+                    <h4 onClick={() => setQ1(!q1)} className={styles.faqQ}><BsQuestionCircleFill className={styles.faqIcon} /> How long does it last? {q1 ? <RxCaretUp className={styles.faqCaret} /> : <RxCaretDown className={styles.faqCaret} />}</h4>
+                    <p style={{maxHeight: q1 ? '35px' : 0, opacity: q1 ? 1 : 0}} className={styles.faqP}>The paint coating should last 5 years, 50,000 miles, or a Lifetime<span className='special-package'>*</span> with our warranty. The coating on any trim, glass, lights, or wheels will typically last about 1 year when taken care of.</p>
                 </li>
 
                 <li className={styles.liContainer}>
                     <h4 onClick={() => setQ4(!q4)} className={styles.faqQ}><BsQuestionCircleFill className={styles.faqIcon} /> How does the 5 year warranty work? {q4 ? <RxCaretUp className={styles.faqCaret} /> : <RxCaretDown className={styles.faqCaret} />}</h4>
-                    <p style={{height: q4 ? 'auto' : 0, opacity: q4 ? 1 : 0}} className={styles.faqP}>If we do our yearly maintenance on the vehicle we guarantee that the ceramic coat will last 5 years or more.</p>
+                    <p style={{maxHeight: q4 ? '35px' : 0, opacity: q4 ? 1 : 0}} className={styles.faqP}>If we do our yearly maintenance on the vehicle we guarantee that the ceramic coat will last 5 years or more.</p>
                 </li>
 
                 <li className={styles.liContainer}>
                     <h4 onClick={() => setQ5(!q5)} className={styles.faqQ}><BsQuestionCircleFill className={styles.faqIcon} /> How does the Lifetime warranty work? {q5 ? <RxCaretUp className={styles.faqCaret} /> : <RxCaretDown className={styles.faqCaret} />}</h4>
-                    <p style={{height: q5 ? 'auto' : 0, opacity: q5 ? 1 : 0}} className={styles.faqP}>Your vehicles paint coating is guaranteed for life as long as you are on our monthly maintenance schedule. If you ever wish to cancel monthly maintenance, the remainder of your 5 year 50,000 mile warranty you had before takes effect.</p>
+                    <p style={{maxHeight: q5 ? '35px' : 0, opacity: q5 ? 1 : 0}} className={styles.faqP}>Your vehicles paint coating is guaranteed for life as long as you are on our monthly maintenance schedule. If you ever wish to cancel monthly maintenance, the remainder of your 5 year 50,000 mile warranty you had before takes effect.</p>
+                </li>
+
+                <li className={styles.liContainer}>
+                    <h4 onClick={() => setQ2(!q2)} className={styles.faqQ}><BsQuestionCircleFill className={styles.faqIcon} /> Can I still go through Automatic car washes? {q2 ? <RxCaretUp className={styles.faqCaret} /> : <RxCaretDown className={styles.faqCaret} />}</h4>
+                    <p style={{maxHeight: q2 ? '35px' : 0, opacity: q2 ? 1 : 0}} className={styles.faqP}>If you want to keep the ceramic coat in good condition and have it last as long as possible, you won't want to go through any car washes, even the touch-less ones.</p>
                 </li>
             </ul>
         </div>
