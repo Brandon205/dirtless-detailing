d1f618b5cfb052be780a848fd489ed83ddfd05f8
Brandon Black <53108034+Brandon205@users.noreply.github.com>
2023-04-19 09:42:56 -0700

    Start of the pricing component

---
+++ utils/PricingComponent.jsx
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from 'react';
+
+{/* <PricingComponent title='Ceramic Coating' prices={[949, 949, 1099, 1099, 1249, 1249]} labels={} /> */}
+export default function PricingComponent(props) {
+    return (
+        <div>
+            <h1>Hello World</h1>
+        </div>
+    )
+}
