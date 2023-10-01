acd0ea1ac89f4bdc5729cc0a5e38109829127f07
Brandon Black <brandonblack02@gmail.com>
2023-09-30 19:27:38 -0700

    Fixed addon bug temporarily

--- utils/Addons.js
+++ utils/Addons.js
@@ -1,16 +1,64 @@
 const addons = {
+    'extraction': {
+        name: 'Steam Extraction',
+        icon: 'extraction',
+        desc: "A quick steam extraction is the best way to remove stains, spills, and embedded dirt that normal vacuuming can't get easily.",
+        cost: ['35', '35', '50', '50', '50', '75']
+    },
+    'hair': {
+        name: 'Pet Hair',
+        icon: 'hair',
+        desc: "Pet hair can be a pain to remove from carpets, luckily we have the tools to get the job done.",
+        cost: ['50-100', '50-100', '50-100', '50-100', '50-100', '50-100']
+    },
+    'headliners': {
+        name: 'Headliners',
+        icon: 'headliners',
+        desc: "Headliners are tricky to clean and improper handling can cause them to sag, we'll pick the right products for the job to clean them while keeping them intact.",
+        cost: ['40-80', '40-80', '40-80', '40-80', '40-80', '40-80']
+    },
+    'intcoating': {
+        name: 'Interior Ceramic Coating',
+        icon: 'intcoating',
+        desc: "Ceramic coating can also be a great option for anyone who wants to protect the materials in their vehicle from stains, UV damage, and to make it easier to clean.",
+        cost: ['150', '150', '150', '150', '150', '150']
+    },
     'ozone': {
         name: 'Ozone Treatment',
         icon: 'ozone',
         desc: "Ozone treatment is the use of O3 (gas ozone) to remove bacteria, viruses, and odors from your vehicle.",
         cost: ['75', '75', '75', '75', '75', '75']
     },
+    'claybar': {
+        name: 'Clay Bar',
+        icon: 'claybar',
+        desc: "Get any small contaminants off of the car to give the paint an even smoother and cleaner shine.",
+        cost: ['35', '35', '50', '50', '75', '75']
+    },
+    'glass': {
+        name: 'Glass Polishing',
+        icon: 'glass',
+        desc: "We will remove as many scratches, etching, and watermarks from the glass as possible.",
+        cost: ['80/hour', '80/hour', '80/hour', '80/hour', '80/hour', '80/hour']
+    },
+    'waterspot': {
+        name: 'Waterspot / Overspray Removal',
+        icon: 'waterspot',
+        desc: "Remove all overspray and waterspots we safely can.",
+        cost: ['80/hour', '80/hour', '80/hour', '80/hour', '80/hour', '80/hour']
+    },
     'engine': {
         name: 'Engine Bay Cleaning',
         icon: 'engine',
         desc: "We will cover sensitive/electronic areas in the bay, clean and wash, then dress the engine bay to have it looking like new!",
         cost: ['75', '75', '75', '75', '75', '75']
-    }
+    },
+    'debadge': {
+        name: 'Debadging',
+        icon: 'debadge',
+        desc: "Remove any vehicle badging including any residue left behind after removal.",
+        cost: ['80/hour', '80/hour', '80/hour', '80/hour', '80/hour', '80/hour']
+    },
 }
 
 export default addons;
