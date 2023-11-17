8c0c5f336fcd9a2a11664954677ffd19c07b82ea
Brandon Black <53108034+Brandon205@users.noreply.github.com>
2023-11-16 22:44:29 -0800

    Animation magic

--- components/FaqItem.jsx
+++ components/FaqItem.jsx
@@ -1,16 +1,17 @@
-import React from 'react';
-import { motion, AnimatePresence } from 'framer-motion';
+import React, { useRef, useEffect} from 'react';
+import { motion } from 'framer-motion';
+import autoAnimate from '@formkit/auto-animate';
 
 export default function FaqItem({i, expanded, setExpanded, title, description}) {
     const isOpen = i === expanded;
+    const parent = useRef(null);
 
-    const openVariants = {
-        open: {opacity: 1, height: "auto"},
-        collapsed: {opacity: 0, height: 0}
-    }
+    useEffect(() => {
+        parent.current && autoAnimate(parent.current)
+    }, [parent])
 
     return (
-        <div key={i}>
+        <div key={i} ref={parent}>
             <div className='faq-title' onClick={() => setExpanded(isOpen ? false : i)}>
                 {title}
 
@@ -21,15 +22,9 @@ export default function FaqItem({i, expanded, setExpanded, title, description}) 
             </div>
 
             {isOpen && (
-                <AnimatePresence>
-                    <motion.div className='faq-description' initial="collapsed" animate="open" exit="collapse" 
-                        variants={openVariants}
-                        transition={{duration: 0.4}}
-                        style={{padding: "0 1rem"}}
-                    >
-                        {description}
-                    </motion.div>
-                </AnimatePresence>
+                <div className='faq-description' style={{padding: "2rem 1rem"}}>
+                    {description}
+                </div>
             )}
         </div>
     )
