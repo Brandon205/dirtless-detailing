ce9d90dd0c50f76e961609743e9771787c7a1062
Brandon Black <53108034+Brandon205@users.noreply.github.com>
2024-02-16 10:07:03 -0800

    Updated copyright year

--- app/Footer.jsx
+++ app/Footer.jsx
@@ -14,7 +14,7 @@ export default function Footer() {
           </div>
         </div>
         <div>
-          <p>©2023 Dirt-Less Detailing.</p>
+          <p>©2024 Dirt-Less Detailing.</p>
           <p>
             Icons made by{" "}
             <a target="_blank" rel="noopener" href="https://icons8.com" className="aside-link">
