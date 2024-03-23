dedc7a6f08de8c501f931d1d0ac31d3424f024ea
Brandon Black <brandonblack02@gmail.com>
2024-03-22 20:46:37 -0700

    temp disable search params

--- app/confirmation/page.tsx
+++ app/confirmation/page.tsx
@@ -1,16 +1,14 @@
 "use client";
 import Image from "next/image";
-import { useSearchParams } from "next/navigation";
+// import { useSearchParams } from "next/navigation";
 import { motion } from "framer-motion";
 import { topBtnVariant } from "../../utils/animationVariations";
-import { Suspense } from "react";
+// import { Suspense } from "react";
 
 export default function Confirmation() {
-  const searchParams = useSearchParams();
+  //   const searchParams = useSearchParams();
 
-  const type = searchParams.get("type");
-
-  console.log(type);
+  //   const type = searchParams.get("type");
 
   return (
     <div className="confirmation-container">
@@ -20,7 +18,7 @@ export default function Confirmation() {
         fill
         style={{ objectFit: "cover", opacity: "25%", zIndex: -5 }}
       />
-      <Suspense>
+      {/* <Suspense>
         {type === "form" ? (
           // Form completed
           <div className="confirmation-subcontainer">
@@ -60,48 +58,47 @@ export default function Confirmation() {
               </motion.a>
             </div>
           </div>
-        ) : (
-          // Easy booking scheduling completed
-          <div className="confirmation-subcontainer">
-            <h1 className="confirmation-title">Thank You for Scheduling!</h1>
-            <p className="confirmation-message">
-              We will confirm your appointment soon! Expect a text from us the
-              night before to confirm the date you selected still works! Feel
-              free to give us a call or text if you have more questions.
-            </p>
-            <div className="header-link-container">
-              <motion.a
-                href="/"
-                className="top-quote-btn"
-                variants={topBtnVariant}
-                whileHover="hover"
-                transition={{ duration: 0.25 }}
-              >
-                <img
-                  src="../assets/icons/home.svg"
-                  alt="home icon"
-                  className="btn-icon"
-                />{" "}
-                Return Home
-              </motion.a>
-              <motion.a
-                href="tel:2532529758"
-                className="top-quote-btn btn-secondary"
-                variants={topBtnVariant}
-                whileHover="hover"
-                transition={{ duration: 0.25 }}
-              >
-                <img
-                  src="../assets/icons/phoneMessage.png"
-                  alt="call or message"
-                  className="btn-icon"
-                />{" "}
-                (253) 252-9758
-              </motion.a>
-            </div>
-          </div>
-        )}
-      </Suspense>
+        ) : ( */}
+      <div className="confirmation-subcontainer">
+        <h1 className="confirmation-title">Thank You for Scheduling!</h1>
+        <p className="confirmation-message">
+          We will confirm your appointment soon! Expect a text from us the night
+          before to confirm the date you selected still works! Feel free to give
+          us a call or text if you have more questions.
+        </p>
+        <div className="header-link-container">
+          <motion.a
+            href="/"
+            className="top-quote-btn"
+            variants={topBtnVariant}
+            whileHover="hover"
+            transition={{ duration: 0.25 }}
+          >
+            <img
+              src="../assets/icons/home.svg"
+              alt="home icon"
+              className="btn-icon"
+            />{" "}
+            Return Home
+          </motion.a>
+          <motion.a
+            href="tel:2532529758"
+            className="top-quote-btn btn-secondary"
+            variants={topBtnVariant}
+            whileHover="hover"
+            transition={{ duration: 0.25 }}
+          >
+            <img
+              src="../assets/icons/phoneMessage.png"
+              alt="call or message"
+              className="btn-icon"
+            />{" "}
+            (253) 252-9758
+          </motion.a>
+        </div>
+      </div>
+      {/* )}
+      </Suspense> */}
     </div>
   );
 }
