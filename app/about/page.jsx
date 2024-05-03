afb3344661817c65aae4ff62577765cadda82ab2
Brandon Black <53108034+Brandon205@users.noreply.github.com>
2024-05-02 19:09:44 -0700

    Wording

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -19,9 +19,8 @@ const jsonld = {
       "name": "What do you need from me when you get here?",
       "acceptedAnswer": {
         "@type": "Answer",
-        "text":
-          "All we will need is an electrical outlet, a water spigot, and preferably all/most personal items out of the car.",
-      },
+        "text": "All we will need is an electrical outlet, a water spigot, and preferably all/most personal items out of the car."
+      }
     },
     {
       "@type": "Question",
@@ -29,8 +28,8 @@ const jsonld = {
       "acceptedAnswer": {
         "@type": "Answer",
         "text":
-          "Every job will take a different amount of time, but once we know the condition of the car and the services requested we can give a good estimate as to how long it will take.",
-      },
+          "Every job will take a different amount of time, but once we know the condition of the car and the services requested we can give a good estimate as to how long it will take."
+      }
     },
     {
       "@type": "Question",
@@ -38,17 +37,16 @@ const jsonld = {
       "acceptedAnswer": {
         "@type": "Answer",
         "text":
-          "It will depend on the amount of space and access if we can or not. Typically apartment complexes and some workplaces can be strict with this type of thing and we may not be able to come to you.",
-      },
+          "It will depend on the amount of space and access if we can or not. Typically apartment complexes and some workplaces can be strict with this type of thing and we may not be able to come to you."
+      }
     },
     {
       "@type": "Question",
       "name": "Do you offer gift cards or gift certificates?",
       "acceptedAnswer": {
         "@type": "Answer",
-        "text":
-          'Yes we do! Any of our services can be bought as a gift certificate. On the Contact page choose the "For Someone Else" option.',
-      },
+        "text": 'Yes we do! Any of our services can be bought as a gift certificate. On the Contact page choose the "For Someone Else" option.'
+      }
     },
     {
       "@type": "Question",
@@ -56,18 +54,18 @@ const jsonld = {
       "acceptedAnswer": {
         "@type": "Answer",
         "text":
-          "With our mobile service we can go out to: Bonney Lake, Buckley, Sumner, Enumclaw, Puyallup, Lake Tapps, Federal Way, Orting, South Prairie, Black Diamond, Milton, Edgewood, and Graham.",
-      },
+          "With our mobile service we can go out to: Bonney Lake, Buckley, Sumner, Enumclaw, Puyallup, Lake Tapps, Federal Way, Orting, South Prairie, Black Diamond, Milton, Edgewood, and Graham."
+      }
     },
     {
       "@type": "Question",
       "name": "Do I have to be there the whole time you're working on my car?",
       "acceptedAnswer": {
         "@type": "Answer",
-        "text": "Absolutely not! You are free to go do whatever you need to do while we are working.",
-      },
-    },
-  ],
+        "text": "Absolutely not! You are free to go do whatever you need to do while we are working."
+      }
+    }
+  ]
 };
 
 export default function About() {
@@ -100,25 +98,25 @@ export default function About() {
     {
       title: "Do you detail boats and RV's?",
       description:
-        "Yes, however, currently we are only doing boat interiors and washes and no polishing for now. Only Washes on RV's if we come to you, we can polish RV's if you can bring it to our shop! Call or text us to get a quote, price is typically a flat fee per foot in length!",
+        "Yes, however, currently we are only doing boat interiors and washes and no polishing for now. Only Washes on RV's if we come to you, we can polish RV's if you can bring it to our shop! Call or text us to get a quote, price is typically a flat fee per foot in length!"
     },
     {
       title: "Can you come to my apartment or workplace?",
       description:
-        "It will depend on the amount of space and water/electrical access if we can or not. Typically apartment complexes and some workplaces can be strict with this type of thing and we may not be able to come to you.",
+        "It will depend on the amount of space and water/electrical access if we can or not. Typically apartment complexes and some workplaces can be strict with this type of thing and we may not be able to come to you."
     },
     {
       title: "Are you licensed and insured?",
-      description: "Yes! We are 100% licensed and insured!",
+      description: "Yes! We are 100% licensed and insured!"
     },
     {
       title: "What cities do you service?",
       description:
-        "With our mobile service we can go out to: Bonney Lake, Buckley, Sumner, Enumclaw, Puyallup, Lake Tapps, Federal Way, Orting, South Prairie, Black Diamond, Milton, Edgewood, and Graham.",
+        "With our mobile service we can go out to: Bonney Lake, Buckley, Sumner, Enumclaw, Puyallup, Lake Tapps, Federal Way, Orting, South Prairie, Black Diamond, Milton, Edgewood, and Graham."
     },
     {
       title: "Do I have to be there the whole time you're working on my car?",
-      description: "Absolutely not! You are free to go do whatever you need to do while we are working.",
+      description: "Absolutely not! You are free to go do whatever you need to do while we are working."
     },
     {
       title: "How would you recommend I wash my car on my own?",
@@ -128,12 +126,11 @@ export default function About() {
           <a href="/products/ceramic-coating" className="aside-link">
             DIY Maintenance
           </a>{" "}
-          page. All the products are perfectly fine to use whether your car is ceramic coated or not! If you are in need
-          of a very quick wash we only recommend touch-less car washes as they won't damage your paint as much as a
-          normal car wash.
+          page. All the products are perfectly fine to use whether your car is ceramic coated or not! If you are in need of a very quick wash we only recommend
+          touch-less car washes as they won't damage your paint as much as a normal car wash.
         </p>
-      ),
-    },
+      )
+    }
   ];
 
   return (
@@ -142,7 +139,7 @@ export default function About() {
       <div
         className="cover"
         style={{
-          backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/a7b4af9c-97db-42dc-3b72-452a6221aa00/cover')`,
+          backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/a7b4af9c-97db-42dc-3b72-452a6221aa00/cover')`
         }}
       ></div>
       <div className="servicepage-header">
@@ -154,22 +151,10 @@ export default function About() {
             Learn a little more about us at Dirt-Less Detailing!
           </h2>
           <div className="header-buttons">
-            <motion.a
-              href="/contact"
-              className="top-quote-btn"
-              variants={topBtnVariant}
-              whileHover="hover"
-              transition={{ duration: 0.25 }}
-            >
+            <motion.a href="/contact" className="top-quote-btn" variants={topBtnVariant} whileHover="hover" transition={{ duration: 0.25 }}>
               <img src="../assets/icons/clipboard.png" alt="clipboard" className="btn-icon" /> Get a Free Quote
             </motion.a>
-            <motion.a
-              href="tel:2532529758"
-              className="top-quote-btn btn-secondary"
-              variants={topBtnVariant}
-              whileHover="hover"
-              transition={{ duration: 0.25 }}
-            >
+            <motion.a href="tel:2532529758" className="top-quote-btn btn-secondary" variants={topBtnVariant} whileHover="hover" transition={{ duration: 0.25 }}>
               <img src="../assets/icons/phoneMessage.png" alt="call or message" className="btn-icon" /> (253) 252-9758
             </motion.a>
           </div>
@@ -183,7 +168,7 @@ export default function About() {
               <div
                 className="about-image"
                 style={{
-                  backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/3a1a1e74-854b-4aa9-ae8e-06816ec80200/public')`,
+                  backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/3a1a1e74-854b-4aa9-ae8e-06816ec80200/public')`
                 }}
               >
                 <img
@@ -195,7 +180,7 @@ export default function About() {
               <div
                 className="about-image"
                 style={{
-                  backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/b5859464-3c0e-4fcf-8332-17155eb86d00/public')`,
+                  backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/b5859464-3c0e-4fcf-8332-17155eb86d00/public')`
                 }}
               >
                 <img
@@ -226,7 +211,7 @@ export default function About() {
                 style={{
                   backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/19851827-a8f8-4045-11cf-a6bdef3c4100/public')`,
                   x: smallScreen ? "" : xMovement,
-                  y: smallScreen ? "" : yMovement,
+                  y: smallScreen ? "" : yMovement
                 }}
               >
                 <img
@@ -240,21 +225,20 @@ export default function About() {
           <div className="text-left">
             <h3 className="text-large">You shouldn't have to take time out of your busy day to clean your car.</h3>
             <p>
-              We can do the dirty work for you, leaving you free to do whatever you desire. After hundreds of completed
-              details over the last 8 years we've seen it all, and perfected how we tackle every aspect of it.
+              We can do the dirty work for you, leaving you free to do whatever you desire. After hundreds of completed details over the last 8 years we've seen
+              it all, and perfected how we tackle every aspect of it.
             </p>
             <p>
-              Starting almost a decade ago as a detailer at a autobody shop, I started Dirt-Less Detailing to bring what
-              I learned from that to everybody near Bonney Lake.
+              Starting almost a decade ago as a detailer at a autobody shop, I started Dirt-Less Detailing to bring all that I learned from then on to everybody
+              near Bonney Lake.
             </p>
             <p>
-              Since I started detailing I've put about as much time detailing in as I have doing research about the
-              products and procedures I use, and I have seen the benefits from my research pay off in every car that we
-              detail.
+              Since I started detailing I've put about as much time detailing in as I have doing research about the products and procedures I use, and I have
+              seen the benefits from my research pay off in every car that we detail.
             </p>
             <p>
-              Whether it's our Swift Interior Detail or a full paint correction and ceramic coating service I can assure
-              you that the differences will be astonishing!
+              Whether it's our Swift Interior Detail or a full paint correction and ceramic coating service I can assure you that the differences will be
+              astonishing!
             </p>
           </div>
         </div>
@@ -265,10 +249,9 @@ export default function About() {
               <p style={{ position: "absolute", margin: 0, transform: "translateX(90%)" }}>K</p>
             </div>
             <p className="about-review">
-              “I cannot believe the hair and grime they were able to get out of my car. Animals, ice cream and makeup
-              spills. Incredible! 150,000 miles and my interior looks brand new. Ozone treatment has it smelling so
-              refreshed. I can't say enough about how impressed I am. On top of that they finished exactly when they
-              said they would and were great to work with. Will definitely use again and refer to friends and family.”
+              “I cannot believe the hair and grime they were able to get out of my car. Animals, ice cream and makeup spills. Incredible! 150,000 miles and my
+              interior looks brand new. Ozone treatment has it smelling so refreshed. I can't say enough about how impressed I am. On top of that they finished
+              exactly when they said they would and were great to work with. Will definitely use again and refer to friends and family.”
             </p>
             <p style={{ color: "#41444A", fontWeight: "bold", fontSize: "20px" }}>Kristen H.</p>
           </div>
@@ -339,7 +322,7 @@ export default function About() {
         <div
           className="about-large-image"
           style={{
-            backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/0d30edc6-6afe-4162-1f9e-4b57f8e85300/public')`,
+            backgroundImage: `url('https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/0d30edc6-6afe-4162-1f9e-4b57f8e85300/public')`
           }}
         >
           <img
@@ -348,21 +331,17 @@ export default function About() {
             src="https://imagedelivery.net/6ELuAqAYnn_KvYt8QhJosQ/0d30edc6-6afe-4162-1f9e-4b57f8e85300/public"
           />
         </div>
-        <h3 style={{ fontSize: "2.5em", margin: 0, textAlign: smallScreen ? "left" : "center" }}>
-          Dirt-Less Detailing
-        </h3>
+        <h3 style={{ fontSize: "2.5em", margin: 0, textAlign: smallScreen ? "left" : "center" }}>Dirt-Less Detailing</h3>
         <div style={{ maxWidth: "1200px", margin: "0 auto" }} className="text-left">
           <p>
-            Dirt-Less Detailing is a premier auto detailing company founded 2 years ago by Brenden (on the right in the
-            picture), an experienced detailer with 8 years of industry knowledge. We specialize in restoring your
-            vehicle's beauty, inside and out. Our mission is simple - to provide you with the best results and customer
-            service that your money can buy. Let us make your car shine like new again!
+            Dirt-Less Detailing is a premier auto detailing company founded 2 years ago by Brenden (on the right in the picture), an experienced detailer with 8
+            years of industry knowledge. We specialize in restoring your vehicle's beauty, inside and out. Our mission is simple - to provide you with the best
+            results and customer service that your money can buy. Let us make your car shine like new again!
           </p>
           <p>
-            At Dirt-Less Detailing, we are passionate about providing top-notch auto detailing services to our valued
-            clients. With our comprehensive range of services, including interior and exterior detailing, ceramic
-            coatings, paint corrections, and engine bay cleanings, we strive to keep your vehicle looking and performing
-            its best. Trust us to provide the care and attention to detail that your car deserves.
+            At Dirt-Less Detailing, we are passionate about providing top-notch auto detailing services to our valued clients. With our comprehensive range of
+            services, including interior and exterior detailing, ceramic coatings, paint corrections, and engine bay cleanings, we strive to keep your vehicle
+            looking and performing its best. Trust us to provide the care and attention to detail that your car deserves.
           </p>
           <p>
             Our shop location is right in the{" "}
@@ -381,15 +360,7 @@ export default function About() {
         <h3 style={{ fontSize: "2.5em" }}>FAQ</h3>
         <div className="faq-container">
           {faqItems.map((item, i) => (
-            <FaqItem
-              key={i}
-              i={i}
-              expanded={expanded}
-              setExpanded={setExpanded}
-              title={item.title}
-              description={item.description}
-              isMobile={smallScreen}
-            />
+            <FaqItem key={i} i={i} expanded={expanded} setExpanded={setExpanded} title={item.title} description={item.description} isMobile={smallScreen} />
           ))}
         </div>
       </section>
