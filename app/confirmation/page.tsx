0ec2fc43f3ad4c05c987d22f280ef46982bd1c35
Brandon Black <53108034+Brandon205@users.noreply.github.com>
2024-10-29 21:13:59 -0700

    Better confirmation message

--- app/confirmation/page.tsx
+++ app/confirmation/page.tsx
@@ -13,10 +13,14 @@ export default function Confirmation() {
         style={{ objectFit: "cover", opacity: "25%", zIndex: -5 }}
       />
       <div className="confirmation-subcontainer">
-        <h1 className="confirmation-title">Thank You for Scheduling!</h1>
-        <p className="confirmation-message">
+        <h1 className="confirmation-title">Thank You for Booking With Us!</h1>
+        <p className="confirmation-message text-xl tracking-wide leading-8">
           We will confirm your appointment soon! Expect a text from us the night before to confirm the date you selected still works! Feel free to give us a
           call or text if you have more questions.
+        </p>
+        <p className="confirmation-message text-md">
+          <span className="special-package">NOTE:</span> We have a 72 hour cancellation policy, get a full refund on your deposit if we're notified 3 days
+          before your appointment.
         </p>
         <div className="header-link-container">
           <motion.a href="/" className="top-quote-btn" variants={topBtnVariant} whileHover="hover" transition={{ duration: 0.25 }}>
