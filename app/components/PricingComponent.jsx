423bef63e497b45ffbc96da728408522ee119ece
Brandon Black <53108034+Brandon205@users.noreply.github.com>
2026-02-27 22:37:33 -0800

    Pricing Changed

--- app/components/PricingComponent.jsx
+++ app/components/PricingComponent.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import prices from "../../utils/Prices";
 import salePrices from "../../utils/SalePrices";
 import { CornerRightDown } from "lucide-react";
--- utils/Prices.js
+++ utils/Prices.js
@@ -1,62 +1,74 @@
 const prices = {
   "Any 2-Door Vehicle": {
     interior: {
-      interior: 335,
-      interiorspill: 500
+      intRefresh: 225,
+      intFull: 335,
+      intRest: 445
     },
     exterior: {
-      exterior: 125,
-      exteriorseal: 600,
-      exteriorcorrect: 1250
+      extDecon: 125,
+      extWashCoat: 500,
+      extPolishCoat: 1000,
+      extCorrectCoat: 1500
     }
   },
   "4 Door": {
     interior: {
-      interior: 375,
-      interiorspill: 550
+      intRefresh: 255,
+      intFull: 375,
+      intRest: 500
     },
     exterior: {
-      exterior: 125,
-      exteriorseal: 600,
-      exteriorcorrect: 1250
+      extDecon: 125,
+      extWashCoat: 500,
+      extPolishCoat: 1000,
+      extCorrectCoat: 1500
     }
   },
   "Mid Size": {
     interior: {
-      interior: 375,
-      interiorspill: 550
+      intRefresh: 255,
+      intFull: 375,
+      intRest: 500
     },
     exterior: {
-      exterior: 150,
-      exteriorseal: 775,
-      exteriorcorrect: 1500
+      extDecon: 125,
+      extWashCoat: 625,
+      extPolishCoat: 1250,
+      extCorrectCoat: 1875
     }
   },
   "Trucks": {
     interior: {
-      interior: 375,
-      interiorspill: 550
+      intRefresh: 255,
+      intFull: 375,
+      intRest: 500
     },
     exterior: {
-      exterior: 175,
-      exteriorseal: 925,
-      exteriorcorrect: 1750
+      extDecon: 125,
+      extWashCoat: 750,
+      extPolishCoat: 1500,
+      extCorrectCoat: 2250
     }
   },
   "3 Row": {
     interior: {
-      interior: 425,
-      interiorspill: 600
+      intRefresh: 285,
+      intFull: 425,
+      intRest: 555
     },
     exterior: {
-      exterior: 175,
-      exteriorseal: 925,
-      exteriorcorrect: 1750
+      extDecon: 125,
+      extWashCoat: 750,
+      extPolishCoat: 1500,
+      extCorrectCoat: 2250
     }
   },
   "addons": {
     "Ozone Treatment": 75,
-    "Engine Bay": 75
+    "Engine Bay": 75,
+    "Interior Ceramic": 75,
+    "Ceramic Maintenance": 75
   }
 };
 
